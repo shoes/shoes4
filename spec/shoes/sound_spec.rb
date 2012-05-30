78f7d1cdd6d4828970b89c64f765b218aa5aaa09
Eric Watson <wasnotrice@gmail.com>
2012-05-30 08:46:48 -0500

    Move sounds to samples/sounds

--- spec/shoes/sound_spec.rb
+++ spec/shoes/sound_spec.rb
@@ -2,7 +2,7 @@ require 'shoes/spec_helper'
 
 describe Shoes::Sound do
   let(:gui_container) { double("gui container") }
-  let(:filepath) { "../../sounds/61847__simon-rue__boink-v3.wav" }
+  let(:filepath) { "../../samples/sounds/61847__simon-rue__boink-v3.wav" }
   subject { Shoes::Sound.new(gui_container, filepath) }
 
   its(:filepath) { should eq(filepath) }
