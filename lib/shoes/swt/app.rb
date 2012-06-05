b238ad88028a5766294857a306dcae37e049524b
Davor Babic <davor@davor.se>
2012-06-06 01:20:02 +0200

    Added support for more background options
    Made it so that "background" doesn't accept symbols anymore, but
    variables.
    
    Added support for top, bottom, left, right.

--- lib/shoes/swt/app.rb
+++ lib/shoes/swt/app.rb
@@ -18,13 +18,12 @@ module Shoes
 
         opts = self.opts
 
-        container.setBackground(COLORS[self.background].to_native)
+        container.setBackground(background.to_native)
         container.setSize(self.width, self.height)
         container.setText(self.title)
 
         container.addListener(::Swt::SWT::Close, main_window_on_close)
       end
-
 
       def gui_open
         self.gui_container.open
@@ -34,10 +33,15 @@ module Shoes
         Shoes.logger.debug "::Swt.display disposed... exiting Shoes::App.new"
       end
 
-      def background(*bg)
-        return @background if bg.empty?
-        self.gui_container.setBackground(COLORS[bg[0]].to_native)
-        @background = bg[0]
+      def background(*opts)
+        return @background if opts.empty?
+        if opts.size == 1
+          puts "only one argument"
+          self.gui_container.setBackground(opts[0].to_native)
+          @background = opts[0]
+        else
+          self.gui_container.addPaintListener(BackgroundPainter.new(opts, self))
+        end
       end
 
       private
@@ -54,6 +58,54 @@ module Shoes
         style |= ::Swt::SWT::RESIZE if opts[:resizable]
 
         style
+      end
+
+      # BackgroundPainter is the thing that paints and repaints the background.
+      # It is only used if the user supplies any parameters other than the color.
+      class BackgroundPainter
+        include org.eclipse.swt.events.PaintListener
+        attr_accessor :options, :color
+
+        def initialize(*opts, app)
+          @app = app
+          self.options = opts[0][1]         
+          self.color   = opts[0][0]
+        end
+
+        def paintControl(e)
+          x        = 0 
+          y        = 0
+          width  = e.width
+          height = e.height
+
+          width  = options[:width]  if options.has_key? :width
+          height = options[:height] if options.has_key? :height
+
+          # top
+          if options.has_key? :top
+            y = options[:top]
+            height -= options[:top]
+          end
+
+          # bottom
+          if options.has_key? :bottom
+            height -= options[:bottom]
+          end
+
+          # left
+          if options.has_key? :left
+            x += options[:left]
+            width -= options[:left]
+          end
+
+          # right
+          if options.has_key? :right
+            width -= options[:right]
+          end
+
+          e.gc.setBackground(self.color.to_native) 
+          e.gc.fillRectangle(x, y, width, height)
+        end
       end
     end
   end
