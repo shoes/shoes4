dc36d852e26b78b3d67f9bb36681f2aad92f93f4
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-06-10 16:20:23 +0200

    specs refactored with the cool test_set_width method

--- spec/swt_shoes/background_painter_spec.rb
+++ spec/swt_shoes/background_painter_spec.rb
@@ -8,6 +8,12 @@ def create_painter(options = {})
   Shoes::Swt::BackgroundPainter.new ["color", options] , nil
 end
 
+# For the given options we expect set_width to return expected
+def test_set_width(expected, options)
+  painter = create_painter(options)
+  painter.set_width(paintEvent).should eq expected
+end
+
 describe Shoes::Swt::BackgroundPainter do
 
   describe "calculating coordines" do
@@ -19,35 +25,29 @@ describe Shoes::Swt::BackgroundPainter do
     describe "set_width" do
 
       it "sets the width to options[:width] if supplied" do
-        painter = create_painter(width: 50)
-        painter.set_width(paintEvent).should eq 50
+        test_set_width(50, width: 50)
       end
       
       it "sets the width to twice the radius if supplied" do
-        painter = create_painter(radius: 25)
-        painter.set_width(paintEvent).should eq 2*25
+        test_set_width(2*25, radius: 25)
       end
       
       it "sets the width to the width option even if radius is supplied" do
-        painter = create_painter(width: 50, radius: 70)
-        painter.set_width(paintEvent).should eq 50
+        test_set_width(50, width: 50, radius: 70)
       end
       
       it "sets the width correctly if right and left are supplied" do
-        painter = create_painter(left: 20, right: 30)
         # the purpose is that left and right are the offsets from the borders
         # the method then computes the distance between those 2 offsets
-        painter.set_width(paintEvent).should eq (APP_WIDTH - 30 -20)
+        test_set_width(APP_WIDTH - (30 + 20), left: 20, right: 30)
       end
       
       it "doest not use left and right if width is also supplied" do
-        painter = create_painter(left: 20, right: 30, width: 70)
-        painter.set_width(paintEvent).should eq 70
+        test_set_width(70, left: 20, right: 30, width: 70)
       end
       
       it "sets the width to the app width if no width parameters are supplied" do
-        painter = create_painter
-        painter.set_width(paintEvent).should eq APP_WIDTH
+        test_set_width(APP_WIDTH, {})
       end
 
     end
