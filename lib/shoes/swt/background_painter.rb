e095acecd329f92c0954dd077a0b9907c3bbd284
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-06-11 15:26:51 +0200

    made paintEvent a nice paint_event - snake_case - sorry

--- lib/shoes/swt/background_painter.rb
+++ lib/shoes/swt/background_painter.rb
@@ -14,65 +14,65 @@ module Shoes
         self.color = options[:fill] if options.has_key? :fill
       end
 
-      def paintControl(paintEvent)
-        coords = calculate_coords paintEvent
-        paintEvent.gc.setBackground(color.to_native)
-        paintEvent.gc.fillRectangle(coords[:x], coords[:y], coords[:width], coords[:height])
+      def paintControl(paint_event)
+        coords = calculate_coords paint_event
+        paint_event.gc.setBackground(color.to_native)
+        paint_event.gc.fillRectangle(coords[:x], coords[:y], coords[:width], coords[:height])
       end
 
-      def calculate_coords(paintEvent)
+      def calculate_coords(paint_event)
         coords = Hash.new
 
-        coords[:width] = set_width(paintEvent)
-        coords[:height] = set_height(paintEvent)
-        coords[:x] = set_x(paintEvent, coords[:width])
-        coords[:y] = set_y(paintEvent, coords[:height])
+        coords[:width] = set_width(paint_event)
+        coords[:height] = set_height(paint_event)
+        coords[:x] = set_x(paint_event, coords[:width])
+        coords[:y] = set_y(paint_event, coords[:height])
 
         coords
       end
 
-      def set_width(paintEvent)
+      def set_width(paint_event)
         if options.has_key? :width
           options[:width]
         elsif options.has_key? :radius
           2*options[:radius]
         elsif options.has_key?(:left) && options.has_key?(:right)
-          paintEvent.width - (options[:right] + options[:left])
+          paint_event.width - (options[:right] + options[:left])
         else
-          paintEvent.width
+          paint_event.width
         end
       end
 
-      def set_height(paintEvent)
+      def set_height(paint_event)
         if options.has_key? :height
           height = options[:height]
         elsif options.has_key? :radius
           2*options[:radius]
         elsif options.has_key?(:top) && options.has_key?(:bottom)
-          paintEvent.height - (options[:top] + options[:bottom])
+          paint_event.height - (options[:top] + options[:bottom])
         else
-          paintEvent.height
+          paint_event.height
         end
       end
 
-      def set_x(paintEvent, width)
+      def set_x(paint_event, width)
         if options.has_key? :left
           options[:left]
-        # if width is != paintEvent.width then it was altered in some way and we
+        # if width is != paint_event.width then it was altered in some way and we
         # have to adjust. Otherwise, 0 is a valid answer.
-        elsif options.has_key?(:right) && (width != paintEvent.width) 
-          paintEvent.width - (width + options[:right])
+        elsif options.has_key?(:right) && (width != paint_event.width)
+          paint_event.width - (width + options[:right])
         else
           0
         end
       end
 
-      def set_y(paintEvent, height)
+      def set_y(paint_event, height)
         if options.has_key? :top
           options[:top]
-        # height is != the paintEvent.height then it was altered and we need to adjust
-        elsif options.has_key?(:bottom) && (height != paintEvent.height)
-          paintEvent.height - (height + options[:bottom])
+        # height is != the paint_event.height then it was altered and we need to adjust
+        elsif options.has_key?(:bottom) && (height != paint_event.height)
+          paint_event.height - (height + options[:bottom])
         else
           0
         end
