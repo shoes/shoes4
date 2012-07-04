8b96a6394fee7d53a68191049f237640d29026c5
Davor <davor@davor.se>
2012-07-04 19:51:22 +0200

    Refactored button, check and radio

--- lib/shoes/swt/button.rb
+++ lib/shoes/swt/button.rb
@@ -1,7 +1,12 @@
+require 'shoes/swt/swt_button'
+
 module Shoes
   module Swt
-    class Button
+    class Button < SwtButton
       include Common::Child
+
+      # The Swt parent object
+      attr_reader :parent
 
       # Create a button
       #
@@ -9,45 +14,14 @@ module Shoes
       # @param [::Swt::Widgets::Composite] parent The parent element of this button
       # @param [Proc] blk The block of code to call when this button is activated
       def initialize(dsl, parent, blk)
-        @dsl = dsl
-        @parent = parent
-        @blk = blk
-        @real = ::Swt::Widgets::Button.new(@parent.real, ::Swt::SWT::PUSH).tap do |button|
-          button.setText(@dsl.text)
-          button.addSelectionListener(@blk) if @blk
-          button.pack
-        end
-      end
-
-      # The Swt parent object
-      attr_reader :parent
-
-      def focus
-        @real.set_focus
+        super(dsl, parent, ::Swt::SWT::PUSH, blk)
+        @real.set_text @dsl.text
       end
 
       def move(left, top)
-        unless @real.disposed?
-          # If this element is part of a layout, we need to pop it into its own
-          # composite layer before moving it, so the rest of of the elements in
-          # the layout can reflow.
-          if @parent.real.get_layout
-            old_parent_real = @parent.real
-            app_real = app.real
-            new_composite = ::Swt::Widgets::Composite.new(app_real, ::Swt::SWT::NO_BACKGROUND)
-            @real.dispose
-            new_composite.set_layout nil
-            @real = ::Swt::Widgets::Button.new(new_composite, ::Swt::SWT::PUSH).tap do |button|
-              button.set_text(@dsl.text)
-              button.add_selection_listener(@blk) if @blk
-              button.pack
-            end
-            new_composite.set_bounds(0, 0, app_real.size.x, app_real.size.y)
-            new_composite.move_above(old_parent_real)
-            old_parent_real.layout
-          end
-          @real.set_location left, top
-          @real.redraw
+        swt_move(left, top) do |button|
+          button.set_text @dsl.text
+          button.add_selection_listener(@blk) if @blk
         end
       end
     end
--- lib/shoes/swt/check.rb
+++ lib/shoes/swt/check.rb
@@ -1,9 +1,11 @@
+require 'shoes/swt/swt_button'
+
 module Shoes
   module Swt
-    class Check
+    class Check < SwtButton
       include Common::Child
 
-      # The Swt parent object
+      # The swt parent object
       attr_reader :parent
 
       # Create a check box
@@ -12,14 +14,7 @@ module Shoes
       # @param [::Swt::Widgets::Composite] parent The parent element of this button
       # @param [Proc] blk The block of code to call when this button is activated
       def initialize(dsl, parent, blk)
-        @dsl = dsl
-        @parent = parent
-        @blk = blk
-        @real = ::Swt::Widgets::Button.new(@parent.real,
-                                ::Swt::SWT::RADIO).tap do |button|
-          button.addSelectionListener(@blk) if @blk
-          button.pack
-        end
+        super(dsl, parent, ::Swt::SWT::CHECK, blk)
       end
 
       def checked?
@@ -30,32 +25,8 @@ module Shoes
         @real.set_selection bool
       end
 
-      def focus
-        @real.set_focus
-      end
-
       def move(left, top)
-        unless @real.disposed?
-          # If this element is part of a layout, we need to pop it into its own
-          # composite layer before moving it, so the rest of of the elements in
-          # the layout can reflow.
-          if @parent.real.get_layout
-            old_parent_real = @parent.real
-            app_real = app.real
-            new_composite = ::Swt::Widgets::Composite.new(app_real, ::Swt::SWT::NO_BACKGROUND)
-            @real.dispose
-            new_composite.set_layout nil
-            @real = ::Swt::Widgets::Button.new(new_composite,
-                        ::Swt::SWT::CHECK).tap do |button|
-              button.pack
-            end
-            new_composite.set_bounds(0, 0, app_real.size.x, app_real.size.y)
-            new_composite.move_above(old_parent_real)
-            old_parent_real.layout
-          end
-          @real.set_location left, top
-          @real.redraw
-        end
+        swt_move(left, top)
       end
     end
   end
