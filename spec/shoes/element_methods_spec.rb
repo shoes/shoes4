7f20c88f0505d2597ad590b117b793c4ed7c0516
Davor <davor@davor.se>
2012-07-05 19:58:45 +0200

    Fixed text_block element method font size specs not running
    As referenced in 2413f5e

--- spec/shoes/element_methods_spec.rb
+++ spec/shoes/element_methods_spec.rb
@@ -1,5 +1,52 @@
 require 'shoes/spec_helper'
 
+
+describe "text_block" do
+  class ElementMethodsShoeLaces
+    attr_accessor :gui
+    include Shoes::ElementMethods
+    def initialize
+      @style = {}
+    end
+  end
+
+  it "should set banner font size to 48" do
+    subject = ElementMethodsShoeLaces.new.banner("hello!")
+    subject.font_size.should eql 48
+  end
+
+  it "should set title font size to 34" do
+    subject = ElementMethodsShoeLaces.new.title("hello!")
+    subject.font_size.should eql 34
+  end
+
+  it "should set subtitle font size to 26" do
+    subject = ElementMethodsShoeLaces.new.subtitle("hello!")
+    subject.font_size.should eql 26
+  end
+
+  it "should set tagline font size to 18" do
+    subject = ElementMethodsShoeLaces.new.tagline("hello!")
+    subject.font_size.should eql 18
+  end
+
+  it "should set caption font size to 14" do
+    subject = ElementMethodsShoeLaces.new.caption("hello!")
+    subject.font_size.should eql 14
+  end
+
+  it "should set para font size to 12" do
+    subject = ElementMethodsShoeLaces.new.para("hello!")
+    subject.font_size.should eql 12
+  end
+
+  it "should set inscription font size to 10" do
+    subject = ElementMethodsShoeLaces.new.inscription("hello!")
+    subject.font_size.should eql 10
+  end
+end
+
+
 shared_examples_for "object with element methods" do
   before(:all) do
     #@gui = Shoes.app do
@@ -10,43 +57,6 @@ shared_examples_for "object with element methods" do
     #    button :text => 'Change to Goodbye', :id => 'button_two'
     #
     #end
-  end
-
-  describe "text block" do
-    it "shold set banner font size to 48" do
-      subject = ElementMethodsShoeLaces.new.banner("hello!")
-      subject.font_size.should eql 48
-    end
-
-    it "shold set title font size to 38" do
-      subject = ElementMethodsShoeLaces.new.title("hello!")
-      subject.font_size.should eql 38
-    end
-
-    it "shold set subtitle font size to 26" do
-      subject = ElementMethodsShoeLaces.new.subtitle("hello!")
-      subject.font_size.should eql 26
-    end
-
-    it "shold set tagline font size to 18" do
-      subject = ElementMethodsShoeLaces.new.tagline("hello!")
-      subject.font_size.should eql 18
-    end
-
-    it "shold set caption font size to 14" do
-      subject = ElementMethodsShoeLaces.new.caption("hello!")
-      subject.font_size.should eql 14
-    end
-
-    it "shold set para font size to 12" do
-      subject = ElementMethodsShoeLaces.new.para("hello!")
-      subject.font_size.should eql 12
-    end
-
-    it "shold set inscription font size to 10" do
-      subject = ElementMethodsShoeLaces.new.inscription("hello!")
-      subject.font_size.should eql 10
-    end
   end
 
   describe "flow" do
