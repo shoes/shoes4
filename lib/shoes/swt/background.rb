1469e756725ca43271c3adb4f107b3fb4cc789a3
ashbb <ashbbb@gmail.com>
2012-08-01 21:16:05 +0900

    Add Shoes::Swt::Common::Resource#gcs_reset method to Background.

--- lib/shoes/swt/background.rb
+++ lib/shoes/swt/background.rb
@@ -16,16 +16,20 @@ module Shoes
       private
 
       class BgPainter
+        include Common::Resource
+
         def initialize(dsl, parent)
           @dsl = dsl
           @parent = parent
         end
 
         def paintControl(paint_event)
+          gc = paint_event.gc
+          gcs_reset gc
           @dsl.width = @dsl.opts[:width] ? @dsl.opts[:width] : @parent.width
           @dsl.height = @dsl.opts[:height] ? @dsl.opts[:height] : @parent.height
-          paint_event.gc.setBackground (@dsl.color.to_native)
-          paint_event.gc.fillRoundRectangle(@parent.left, @parent.top, 
+          gc.setBackground (@dsl.color.to_native)
+          gc.fillRoundRectangle(@parent.left, @parent.top, 
             @dsl.width, @dsl.height, @dsl.curve*2, @dsl.curve*2)
         end
       end
