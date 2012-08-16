ee92deaf0ae101bb62213a3a93e9a035515bf481
Eric Watson <wasnotrice@gmail.com>
2012-08-16 11:29:18 -0500

    Arc: remove duplication in @width, @height
    ElementMethods#width and #height delegate to @gui, so there's
    no reason to store width and height as instance variables in
    the DSL class.

--- lib/shoes/arc.rb
+++ lib/shoes/arc.rb
@@ -6,13 +6,13 @@ module Shoes
     include Common::Style
 
     def initialize(left, top, width, height, angle1, angle2, opts = {})
-      @left, @top, @width, @height = left, top, width, height
+      @left, @top = left, top
       #@angle1, @angle2 = angle1, angle2
       default_style = Common::Fill::DEFAULTS.merge(Common::Stroke::DEFAULTS)
       @style = default_style.merge(opts)
 
       #GUI
-      gui_opts = {:width => @width, :height => @height}
+      gui_opts = {:width => width, :height => height}
       @gui = Shoes.configuration.backend_for(self, gui_opts)
     end
   end
