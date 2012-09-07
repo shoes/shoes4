7074f89efc1e36440612017523931e192b04f019
Eric Watson <wasnotrice@gmail.com>
2012-09-07 14:40:29 -0500

    Stub Swt#event_loop in all Swt specs

--- spec/swt_shoes/spec_helper.rb
+++ spec/swt_shoes/spec_helper.rb
@@ -3,5 +3,11 @@ require "shoes/swt"
 
 Shoes.configuration.backend = :swt
 
+RSpec.configure do |config|
+  config.before(:each) do
+    Swt.stub(:event_loop)
+  end
+end
+
 shared_examples = File.join(File.expand_path(File.dirname(__FILE__)), 'shared_examples', '**/*.rb')
 Dir[shared_examples].each { |f| require f }
