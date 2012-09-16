21453c4188a32dce8becd64fbfa2691a090847f8
Eric Watson <wasnotrice@gmail.com>
2012-09-16 14:42:44 -0500

    Update Check specs to work with Swt backend

--- spec/shoes/check_spec.rb
+++ spec/shoes/check_spec.rb
@@ -4,12 +4,10 @@ describe Shoes::Check do
   subject { Shoes::Check.new(parent, input_opts, input_block) }
   let(:input_block) { Proc.new {} }
   let(:input_opts) { {} }
-  let(:parent) { double("parent").as_null_object }
+  let(:app) { Shoes::App.new }
+  let(:parent) { Shoes::Flow.new app }
 
-  it "should respond to the right methods" do
-    s = subject
-    s.should respond_to :checked=
-    s.should respond_to :checked?
-    s.should respond_to :focus
-  end
+  it { should respond_to :checked= }
+  it { should respond_to :checked? }
+  it { should respond_to :focus }
 end
