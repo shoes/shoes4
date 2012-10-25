3620ec29f7780b1e7fc70c048036456a6b69887d
Eric Watson <wasnotrice@gmail.com>
2012-10-25 00:05:12 -0500

    Remove unused code

--- lib/shoes/package.rb
+++ lib/shoes/package.rb
@@ -1,14 +1,5 @@
 module Shoes
   module Package
-    class P
-      def initialize(backend, wrapper)
-        @backend, @wrapper = backend, wrapper
-      end
-
-      def package
-        puts "Packaging as #{@backend}:#{@wrapper}"
-      end
-    end
     def self.new(backend, wrapper, config)
       # Belongs in Shoes::Configuration
       require "shoes/#{backend.to_s.downcase}"
@@ -20,7 +11,6 @@ module Shoes
       klass.new config
     rescue LoadError => e
       raise LoadError, "Couldn't load backend '#{backend}'. Error: #{e.message}\n#{e.backtrace.join("\n")}"
-      P.new(backend, wrapper)
     end
   end
 end
