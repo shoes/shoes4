044624c242e51dad87cd72ab07f4684a6576f800
Eric Watson <wasnotrice@gmail.com>
2012-11-15 20:50:05 -0600

    Add hex string handling to Color

--- spec/shoes/color_spec.rb
+++ spec/shoes/color_spec.rb
@@ -11,144 +11,196 @@ describe Shoes::Color do
     it { should_not be_white }
   end
 
-  context "black" do
-    context "with optional alpha" do
-      subject { Shoes::Color.new(0, 0, 0, 0) }
-      it_behaves_like "black"
-      its(:alpha) { should eq(0) }
+  shared_examples "color with bad arguments" do
+    it "raises ArgumentError" do
+      subject.should raise_error(ArgumentError)
+    end
+  end
+
+  context "with wrong number of arguments" do
+    subject { lambda { Shoes::Color.new(10, 10) } }
+    it_behaves_like "color with bad arguments"
+  end
+
+  context "with too many hex chars" do
+    subject { lambda { Shoes::Color.new("a1b2c3d") } }
+    it_behaves_like "color with bad arguments"
+  end
+
+  context "with too few hex chars" do
+    subject { lambda { Shoes::Color.new("a1") } }
+    it_behaves_like "color with bad arguments"
+  end
+
+  context "with invalid hex chars" do
+    subject { lambda { Shoes::Color.new("#g01234") } }
+    it_behaves_like "color with bad arguments"
+  end
+
+  context "hex" do
+    let(:rgb) { Shoes::Color.new(0, 0, 0, 255) }
+
+    context "with '#000000'" do
+      subject { Shoes::Color.new("#000000") }
+      it { should eq(rgb) }
     end
 
-    context "without optional alpha" do
-      subject { Shoes::Color.new(0, 0, 0) }
-      it_behaves_like "black"
-      its(:alpha) { should eq(255) }
+    context "with '000000'" do
+      subject { Shoes::Color.new("000000") }
+      it { should eq(rgb) }
     end
 
-    context "using floats" do
+    context "with '000'" do
+      subject { Shoes::Color.new("000") }
+      it { should eq(rgb) }
+    end
+
+    context "with '#000'" do
+      subject { Shoes::Color.new("#000") }
+      it { should eq(rgb) }
+    end
+  end
+
+  context "rgb" do
+    context "black" do
       context "with optional alpha" do
-        subject { Shoes::Color.new(0.0, 0.0, 0.0, 0.0) }
+        subject { Shoes::Color.new(0, 0, 0, 0) }
         it_behaves_like "black"
         its(:alpha) { should eq(0) }
       end
 
       context "without optional alpha" do
-        subject { Shoes::Color.new(0.0, 0.0, 0.0) }
+        subject { Shoes::Color.new(0, 0, 0) }
         it_behaves_like "black"
         its(:alpha) { should eq(255) }
       end
+
+      context "using floats" do
+        context "with optional alpha" do
+          subject { Shoes::Color.new(0.0, 0.0, 0.0, 0.0) }
+          it_behaves_like "black"
+          its(:alpha) { should eq(0) }
+        end
+
+        context "without optional alpha" do
+          subject { Shoes::Color.new(0.0, 0.0, 0.0) }
+          it_behaves_like "black"
+          its(:alpha) { should eq(255) }
+        end
+      end
     end
-  end
 
-  context "white" do
-    subject { Shoes::Color.new(255, 255, 255) }
-    it { should be_white }
-    it { should_not be_black }
-  end
-
-  context "peru" do
-    shared_examples_for "peru" do
-      its(:class) { should eq(Shoes::Color) }
-      its(:red) { should eq(205) }
-      its(:green) { should eq(133) }
-      its(:blue) { should eq(63) }
+    context "white" do
+      subject { Shoes::Color.new(255, 255, 255) }
+      it { should be_white }
       it { should_not be_black }
-      it { should_not be_white }
     end
 
-    context "with optional alpha" do
-      subject { Shoes::Color.new(205, 133, 63, 100) }
-      it_behaves_like("peru")
-      its(:alpha) { should eq(100) }
-    end
-
-    context "without optional alpha" do
-      subject { Shoes::Color.new(205, 133, 63) }
-      it_behaves_like("peru")
-      its(:alpha) { should eq(255) }
-    end
-
-    context "using floats" do
-      let(:red) { 0.805 }
-      let(:green) { 0.52 }
-      let(:blue) { 0.248 }
-      let(:alpha) { 0.392 }
+    context "peru" do
+      shared_examples_for "peru" do
+        its(:class) { should eq(Shoes::Color) }
+        its(:red) { should eq(205) }
+        its(:green) { should eq(133) }
+        its(:blue) { should eq(63) }
+        it { should_not be_black }
+        it { should_not be_white }
+      end
 
       context "with optional alpha" do
-        subject { Shoes::Color.new(red, green, blue, alpha) }
-        it_behaves_like "peru"
+        subject { Shoes::Color.new(205, 133, 63, 100) }
+        it_behaves_like("peru")
         its(:alpha) { should eq(100) }
       end
 
       context "without optional alpha" do
-        subject { Shoes::Color.new(red, green, blue) }
-        it_behaves_like "peru"
+        subject { Shoes::Color.new(205, 133, 63) }
+        it_behaves_like("peru")
         its(:alpha) { should eq(255) }
       end
-    end
-  end
 
-  describe "light and dark" do
-    let(:lightgreen) { Shoes::Color.new(144, 238, 144) }
-    let(:darkgreen) { Shoes::Color.new(0, 100, 0) }
-    let(:mediumseagreen) { Shoes::Color.new(60, 179, 113) }
+      context "using floats" do
+        let(:red) { 0.805 }
+        let(:green) { 0.52 }
+        let(:blue) { 0.248 }
+        let(:alpha) { 0.392 }
 
-    specify "light color is light" do
-      lightgreen.should be_light
-      mediumseagreen.should_not be_light
-      darkgreen.should_not be_light
-    end
+        context "with optional alpha" do
+          subject { Shoes::Color.new(red, green, blue, alpha) }
+          it_behaves_like "peru"
+          its(:alpha) { should eq(100) }
+        end
 
-    specify "dark color is dark" do
-      lightgreen.should_not be_dark
-      mediumseagreen.should_not be_dark
-      darkgreen.should be_dark
-    end
-  end
-
-  describe "transparency" do
-    let(:transparent) { Shoes::Color.new(25, 25, 112, 0) }
-    let(:semi) { Shoes::Color.new(25, 25, 112, 100) }
-    let(:opaque) { Shoes::Color.new(25, 25, 25) }
-
-    specify "only transparent colors are transparent" do
-      transparent.should be_transparent
-      semi.should_not be_transparent
-      opaque.should_not be_transparent
+        context "without optional alpha" do
+          subject { Shoes::Color.new(red, green, blue) }
+          it_behaves_like "peru"
+          its(:alpha) { should eq(255) }
+        end
+      end
     end
 
-    specify "only opaque colors should be opaque" do
-      transparent.should_not be_opaque
-      semi.should_not be_opaque
-      opaque.should be_opaque
-    end
-  end
+    describe "light and dark" do
+      let(:lightgreen) { Shoes::Color.new(144, 238, 144) }
+      let(:darkgreen) { Shoes::Color.new(0, 100, 0) }
+      let(:mediumseagreen) { Shoes::Color.new(60, 179, 113) }
 
-  describe "comparable" do
-    let(:color_1) { Shoes::Color.new(255, 69, 0) } # orangered
-
-    it "is equal when values are equal" do
-      color_2 = Shoes::Color.new(255, 69, 0)
-      color_1.should eq(color_2)
-    end
-
-    it "is less than when darker" do
-      color_2 = Shoes::Color.new(255, 70, 0)
-      color_1.should be < color_2
-    end
-
-    it "is greater than when lighter" do
-      color_2 = Shoes::Color.new(255, 68, 0)
-      color_1.should be > color_2
-    end
-
-    context "same rgb values" do
-      let(:color_2) { Shoes::Color.new(255, 69, 0, 254) }
-      it "is less than when less opaque" do
-        color_2.should be < color_1
+      specify "light color is light" do
+        lightgreen.should be_light
+        mediumseagreen.should_not be_light
+        darkgreen.should_not be_light
       end
 
-      it "is greater than when more opaque" do
+      specify "dark color is dark" do
+        lightgreen.should_not be_dark
+        mediumseagreen.should_not be_dark
+        darkgreen.should be_dark
+      end
+    end
+
+    describe "transparency" do
+      let(:transparent) { Shoes::Color.new(25, 25, 112, 0) }
+      let(:semi) { Shoes::Color.new(25, 25, 112, 100) }
+      let(:opaque) { Shoes::Color.new(25, 25, 25) }
+
+      specify "only transparent colors are transparent" do
+        transparent.should be_transparent
+        semi.should_not be_transparent
+        opaque.should_not be_transparent
+      end
+
+      specify "only opaque colors should be opaque" do
+        transparent.should_not be_opaque
+        semi.should_not be_opaque
+        opaque.should be_opaque
+      end
+    end
+
+    describe "comparable" do
+      let(:color_1) { Shoes::Color.new(255, 69, 0) } # orangered
+
+      it "is equal when values are equal" do
+        color_2 = Shoes::Color.new(255, 69, 0)
+        color_1.should eq(color_2)
+      end
+
+      it "is less than when darker" do
+        color_2 = Shoes::Color.new(255, 70, 0)
+        color_1.should be < color_2
+      end
+
+      it "is greater than when lighter" do
+        color_2 = Shoes::Color.new(255, 68, 0)
         color_1.should be > color_2
+      end
+
+      context "same rgb values" do
+        let(:color_2) { Shoes::Color.new(255, 69, 0, 254) }
+        it "is less than when less opaque" do
+          color_2.should be < color_1
+        end
+
+        it "is greater than when more opaque" do
+          color_1.should be > color_2
+        end
       end
     end
   end
