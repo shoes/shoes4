826a5c08940b71de3ddc2c1ad32ea9fa8364dc29
Eric Watson <wasnotrice@gmail.com>
2012-12-31 08:16:28 -0600

    Report dimensions in the app, not console

--- samples/simple-app-dimensions.rb
+++ samples/simple-app-dimensions.rb
@@ -1,6 +1,9 @@
 Shoes.app do
-  button 'report' do
-    puts width.to_s
-    puts height.to_s
+  stack margin: 20 do
+    @dimensions = title "width: #{width}, height: #{height} (default)"
+    para "Resize the window and click 'report' to show new dimensions"
+    button 'report' do
+      @dimensions.replace "width: #{width}, height: #{height}"
+    end
   end
 end
