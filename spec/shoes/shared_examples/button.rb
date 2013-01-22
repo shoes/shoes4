72f2d4b10815fdb80dcf06199704564b9aa94294
Davor Babic <davor@davor.se>
2013-01-22 04:04:45 +0100

    Button, Check and Radio now respond to click(). Aditionally, button_spec checks that Button responds to focus().

--- spec/shoes/shared_examples/button.rb
+++ spec/shoes/shared_examples/button.rb
@@ -2,6 +2,7 @@ shared_examples "checkable" do
   it { should respond_to :checked= }
   it { should respond_to :checked? }
   it { should respond_to :focus }
+  it { should respond_to :click }
 end
 
 shared_examples "buttons" do
