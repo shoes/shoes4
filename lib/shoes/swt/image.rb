5979e97a071de17b4d944bbbe80790b62c2b49fe
hkal <_@hkal.me>
2013-06-15 09:49:12 -0400

    Implemented Image#path.
    * refer to #252
    * fixes #252
    * i'm not certain how to write tests for this, but i'm certain it needs some

--- lib/shoes/swt/image.rb
+++ lib/shoes/swt/image.rb
@@ -17,9 +17,22 @@ module Shoes
         @left, @top = @dsl.left, @dsl.top
         @container = @parent.real
 
-        name = @dsl.file_path
+        load_image(@dsl.file_path)
+
+        parent.dsl.contents << @dsl
+
+        @painter = lambda do |event|
+          gc = event.gc
+          gc.drawImage @real, 0, 0, @full_width, @full_height, @left, @top, @width, @height unless @dsl.hidden
+        end
+        @container.add_paint_listener(@painter)
+
+        clickable self, blk
+      end
+
+      def load_image(name)
         if name =~ /^(http|https):\/\//
-          tmpname = File.join(Dir.tmpdir, "__shoes4_#{Time.now.to_f}.png")
+          @tmpname = File.join(Dir.tmpdir, "__shoes4_#{Time.now.to_f}.png") unless @tmpname
           url, name = name, File.join(DIR, 'static/downloading.png')
         end
 
@@ -28,24 +41,16 @@ module Shoes
         @width = @dsl.opts[:width] || @full_width
         @height = @dsl.opts[:height] || @full_height
 
-        @dsl.app.download url, save: tmpname do
-          tmp = ::Swt::Graphics::Image.new Shoes.display, tmpname
-          @real = tmp
-          @full_width, @full_height = tmp.getImageData.width, tmp.getImageData.height
+        @dsl.app.download url, save: @tmpname do
+          @real = ::Swt::Graphics::Image.new Shoes.display, @tmpname
+          @full_width, @full_height = @real.getImageData.width, @real.getImageData.height
           @width = @dsl.opts[:width] || @full_width
           @height = @dsl.opts[:height] || @full_height
         end if url
+      end
 
-        parent.dsl.contents << @dsl
-
-        @painter = lambda do |event|
-          gc = event.gc
-          gcs_reset gc
-          gc.drawImage @real, 0, 0, @full_width, @full_height, @left, @top, @width, @height unless @dsl.hidden
-        end
-        @container.add_paint_listener(@painter)
-
-        clickable self, blk
+      def update_image
+        load_image(@dsl.file_path)
       end
     end
   end
