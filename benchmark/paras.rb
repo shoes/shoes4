a26edd58c649787aa98049000abfe417cacb08f3
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-06-26 20:49:47 +0200

    para benchmark reworked

--- benchmark/paras.rb
+++ benchmark/paras.rb
@@ -3,23 +3,26 @@ require 'benchmark'
 LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est"
 
 Shoes.app do
-  Benchmark.bm do |benchmark|
-    benchmark.report '100' do
-      100.times do
-        para LOREM_IPSUM
+
+  def benchmark_paras
+    Benchmark.bm do |benchmark|
+      benchmark.report '100' do
+        create_paras 100
+        gui.flush
       end
 
       benchmark.report '1000' do
-        1000.times do
-          para LOREM_IPSUM
-        end
-      end
-
-      benchmark.report '10000' do
-        1000.times do
-          para LOREM_IPSUM
-        end
+        create_paras 1000
+        gui.flush
       end
     end
+  end
+
+  def create_paras(count)
+    count.times do para LOREM_IPSUM end
+  end
+
+  button 'start benchmarking' do
+    benchmark_paras
   end
 end
