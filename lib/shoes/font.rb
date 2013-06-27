76240eea883f1ce1d8881d8853235fe0b44a4ce1
Todd Pickell <myappleguy@gmail.com>
2013-06-27 17:51:42 -0500

    working on font class for next pull request

--- lib/shoes/font.rb
+++ lib/shoes/font.rb
@@ -3,8 +3,7 @@ module Shoes
 
     # TODO-refactor this to have better names for methods and variables
   class Font
-    FONT_TYPES = "{ttf,ttc,otf,fnt,fon,bdf,pcf,snf,mmm,pfb,pfm}"
-    attr_reader :path, :font_name
+    attr_reader :font_path, :font_name
 
     def self.fonts_from_dir(path)
       font_names_and_paths = {}
@@ -36,60 +35,32 @@ module Shoes
       Shoes::FONTS.flatten!
     end
 
-    def self.parse_filename_from_path(file_path)
+
+      # TODO-check for font name or path here this assumes good input
+    def initialize(font_path = '')
+      @font_path = font_path
+      @font_name = remove_file_ext(parse_filename_from_path(@font_path))
+      self.class.add_font_names_to_fonts_constant
+    end
+
+    def parse_filename_from_path(file_path)
       Pathname.new(file_path).basename.to_s
     end
 
-    def self.remove_file_ext(file_name)
+    def remove_file_ext(file_name)
       file_name.chomp(File.extname(file_name))
     end
 
-      # TODO-check for font name or path here this assumes good input
-    def initialize(path = '')
-      @path = path
-      self.class.add_font_names_to_fonts_constant
-      @found = find_font
-    end
-
-    def find_font
-      return false unless available?
-      return true if in_folder?
-      load_font_from_system
-    end
-
-    def available?
-      FONTS.include? @path
-    end
-
-    def in_folder?
-      self.class.fonts_from_dir(FONT_DIR).include? @path
-    end
-
-    # TODO-refactor to pull something out of this it is too big
-    def load_font_from_system
-      fonts_hash = {}
-      self.class.system_font_dirs.each do |dir|
-        fonts = self.class.fonts_from_dir(dir)
-        fonts_hash.merge!(fonts)
-      end
-      copy_file_to_font_folder(fonts_hash.fetch(@path))
-    end
-
-    def copy_file_to_font_folder(file_path)
-      new_file_path = Shoes::FONT_DIR + self.class.parse_filename_from_path(file_path)
-      FileUtils.cp(file_path, Shoes::FONT_DIR)
-      FileUtils.cmp(file_path, new_file_path)
-    end
-
-    def found?
-      @found
-    end
-
-      # TODO-need to work on return value from what is called in builtins
-      # TODO-needs to pass tests in textblock spec still after refactor
     def load_font
       return @font_name unless @path == ''
       DEFAULT_TEXTBLOCK_FONT
     end
   end
 end
+
+__END__
+take a path in
+grab the font name from the path
+store font name and path in hash
+update FONTS with newly loaded font name
+
