99ad88ba51c6e8183d966996b0dcefbfbdca7d3d
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-07-04 13:07:16 +0200

    Preliminary version fo the refactoring to use the Swt font impl
    * do not check this commit out due to test not passing issues
    * see #313

--- lib/shoes/font.rb
+++ lib/shoes/font.rb
@@ -8,34 +8,31 @@ class Shoes
     class << self
       attr_reader :loaded_fonts
 
-      def add_font_names_to_fonts_constant
-        Shoes::FONTS.clear
-        all_font_source_dirs = system_font_dirs << FONT_DIR
-        all_font_source_dirs.each{|dir| load_fonts_from_dir(dir)}
-        Shoes::FONTS << @loaded_fonts.keys
-        Shoes::FONTS.flatten!
-      end
-
-      def load_fonts_from_dir(path)
-        Dir.glob(path + "**/*." + FONT_TYPES).each do |font_file|
-          add_font(font_file)
+      def font_paths_from_dir(path)
+        font_paths = []
+        Dir.glob(path + "**/*." + FONT_TYPES).each do |font_path|
+          font_paths << font_path
         end
+        font_paths
       end
 
       def add_font(path)
-        path = path
-        name = remove_file_ext(parse_filename_from_path(path))
-        load_font(name, path) unless font_already_loaded?(name)
+        Shoes.backend::Font.add_font(path)
+      end
+
+      def add_font_to_fonts(path)
+        name = font_name(path)
+        Shoes::FONTS << name
         name
       end
 
-      def load_font(name, path)
-        loaded_fonts[name] = path
-        Shoes::FONTS << name
+      def initial_fonts
+        Shoes.backend::Font.initial_fonts
       end
 
-      def font_already_loaded?(font_name)
-        loaded_fonts.include?(font_name) || Shoes::FONTS.include?(font_name)
+      private
+      def font_name(path)
+        remove_file_ext(parse_filename_from_path(path))
       end
 
       def parse_filename_from_path(file_path)
@@ -45,25 +42,8 @@ class Shoes
       def remove_file_ext(file_name)
         file_name.chomp(File.extname(file_name))
       end
-
-      def system_font_dirs
-        case RbConfig::CONFIG['host_os']
-          when "darwin"
-            ["/System/Library/Fonts/", "/Library/Fonts/" ]
-          when "linux", "linux-gnu"
-            ["/usr/share/fonts/" , "/usr/local/share/fonts/", Dir.home + "/.fonts/"]
-          when /mswin/, "windows", "mingw"
-            ["/Windows/Fonts/"]
-          else
-            raise RuntimeError, "Undetermined Host OS"
-        end
-      end
-
     end
-
-    # as soon as this file is loaded populate the FONTS array with fonts
-    # maybe we need something like boot.rb for this?
-    add_font_names_to_fonts_constant
-
   end
+
+  FONTS = Shoes::Font.initial_fonts
 end
---
+++ lib/shoes/mock/font.rb
@@ -0,0 +1,17 @@
+class Shoes
+  module Mock
+    module Font
+
+      class << self
+        def add_font(path)
+          Shoes::Font.add_font_to_fonts path
+        end
+
+        def initial_fonts
+          []
+        end
+
+      end
+    end
+  end
+end
