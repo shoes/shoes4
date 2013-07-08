419127fb6707d28cd5526aad20433e06705865cb
ashbb <ashbbb@gmail.com>
2013-07-08 23:53:47 +0900

    Modify sample45.rb to fit #314

--- samples/sample45.rb
+++ samples/sample45.rb
@@ -1,10 +1,9 @@
 Shoes.app height: 200 do
-  title 'Hello Green Shoes'
-  font 'Coolvetica'
-  title 'Hello Green Shoes'
-  title 'Hello Green Shoes', font: 'Lucida console'
-  font nil
+  font File.join(Shoes::DIR, 'fonts/Coolvetica.ttf')
+  title 'Good Question'
+  title 'Good Question', font: 'Coolvetica'
+  title 'Good Question', font: 'Lucida console'
   para 'Arial (default)', left: 400, top: 25
   para 'Coolvetica', left: 400, top: 70
-  para 'Lucida Console', left: 470, top: 120
+  para 'Lucida Console', left: 400, top: 120
 end
