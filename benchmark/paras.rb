418948fb98934e07692726c9cb9be1c0014b58a3
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-07-08 13:11:40 +0200

    Extracted a para_creater module

--- benchmark/paras.rb
+++ benchmark/paras.rb
@@ -1,8 +1,8 @@
 require 'benchmark'
-
-LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est"
+require File.dirname(__FILE__) + '/para_creator'
 
 Shoes.app do
+  extend ParaCreator
 
   def benchmark_paras
     Benchmark.bm do |benchmark|
@@ -33,23 +33,14 @@ Shoes.app do
     count.times do
       stack do
         flow do
-          para LOREM_IPSUM
+          para ParaCreator::LOREM_IPSUM
         end
         flow do
-          para LOREM_IPSUM
+          para ParaCreator::LOREM_IPSUM
         end
       end
     end
     flush_gui
-  end
-
-  def create_paras(count)
-    count.times do para LOREM_IPSUM end
-    flush_gui
-  end
-
-  def flush_gui
-    gui.flush if respond_to? :gui
   end
 
   button 'benchmark paras' do
