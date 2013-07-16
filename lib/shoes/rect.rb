06d46bdbaaf87fda356d2097d1559058aa73f61d
Faraaz Ismail <fism88@gmail.com>
2013-07-16 14:37:15 -0500

    Click option for rectangle

--- lib/shoes/rect.rb
+++ lib/shoes/rect.rb
@@ -18,6 +18,7 @@ class Shoes
       @app.unslotted_elements << self
 
       @gui = Shoes.backend_for(self, left, top, width, height, opts, &blk)
+      click(&opts[:click]) if opts[:click]
     end
 
     attr_reader :app, :hidden
