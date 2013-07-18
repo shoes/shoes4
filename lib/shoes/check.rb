8ad9bdc4c0b82125deffeb9c879c930ed3afe9b1
Faraaz Ismail <fism88@gmail.com>
2013-07-18 14:12:25 -0500

    Add options back to Check

--- lib/shoes/check.rb
+++ lib/shoes/check.rb
@@ -1,18 +1,21 @@
 class Shoes
   class Check
     include Shoes::CommonMethods
+    include Shoes::Common::Clickable
 
     attr_reader :parent
     attr_reader :blk
     attr_reader :gui
 
-    def initialize(app, parent, blk = nil)
+    def initialize(app, parent, opts = {}, blk = nil)
       @app    = app
       @parent = parent
       @blk    = blk
 
       @gui = Shoes.configuration.backend_for(self, @parent.gui, blk)
       @parent.add_child self
+
+      clickable_options(opts)
     end
 
     def checked?
