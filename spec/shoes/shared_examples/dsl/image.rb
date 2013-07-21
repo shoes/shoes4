531683c77e643f7f1bb78db92a11a4a247cc1ab2
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-07-21 17:00:43 +0200

    Removed backend_is :mock from the shared image spec
    * we want to tes with all the backends
    * replaced path with shoes icon

--- spec/shoes/shared_examples/dsl/image.rb
+++ spec/shoes/shared_examples/dsl/image.rb
@@ -1,23 +1,21 @@
 shared_examples_for "image DSL method" do
-  let(:path)  { '/some/path' }
+  let(:path)  { Shoes::DIR + '/static/shoes-icon.png' }
   let(:opts)  { {} }
   let(:image) { dsl.image path, opts }
 
-  backend_is :mock do
-    it 'should set the path' do
-      image.file_path.should == path
+  it 'should set the path' do
+    image.file_path.should == path
+  end
+
+  context 'with :top and :left style attributes' do
+    let(:opts) { {left: 55, top: 66} }
+
+    it 'should set the left' do
+      image.left.should == 55
     end
 
-    context 'with :top and :left style attributes' do
-      let(:opts) { {left: 55, top: 66} }
-
-      it 'should set the left' do
-        image.left.should == 55
-      end
-
-      it 'should set the top' do
-        image.top.should == 66
-      end
+    it 'should set the top' do
+      image.top.should == 66
     end
   end
 end
