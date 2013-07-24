51d8e6e1ad4f49c0cbb5d8ad95946a0f61037c8b
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-07-24 12:27:34 +0200

    no more reference to the Mock Backend in the oval spec

--- spec/shoes/oval_spec.rb
+++ spec/shoes/oval_spec.rb
@@ -3,11 +3,6 @@ require 'shoes/spec_helper'
 describe Shoes::Oval do
   let(:app) { Shoes::App.new }
 
-  before :each do
-    Shoes::Mock::Oval.any_instance.stub(:real) { double( size:
-      double(x: 100, y: 100) ) }
-  end
-
   describe "basic" do
     subject { Shoes::Oval.new(app, 20, 30, 100, 200) }
     it_behaves_like "object with stroke"
