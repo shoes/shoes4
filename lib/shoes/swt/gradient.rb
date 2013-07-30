568af519967133ec30148f969b425dedbf897340
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-07-30 15:52:59 +0200

    Further refactoring on gradient
    * corrected some names (e.g. pattern_position --> create pattern)
    * extracted further methods, that belong together
    * opening PR as request for Feedback/Help :-)

--- lib/shoes/swt/gradient.rb
+++ lib/shoes/swt/gradient.rb
@@ -18,27 +18,31 @@ class Shoes
       end
 
       def apply_as_fill(gc, left, top, width, height, angle = 0)
-        pattern = pattern_position(left, top, width, height, -angle)
+        pattern = create_pattern(left, top, width, height, -angle)
         gc.set_background_pattern pattern
       end
 
       def apply_as_stroke(gc, left, top, width, height, angle = 0)
-        pattern = pattern_position(left, top, width, height, -angle)
+        pattern = create_pattern(left, top, width, height, -angle)
         gc.set_foreground_pattern pattern
       end
 
-      def pattern_position left, top, width, height, angle
-        width, height = width*0.5, height*0.5
-        angle = Math::PI*(angle/180.0)
-        angle = angle % (Math::PI*2.0)
-        cal = proc do
-          left = Math.sqrt(width**2 + height**2)
-          b = (height==0 and width==0) ? height/width : Math.atan(height/width)
-          c = Math::PI*0.5 - angle - b
-          r = left * Math.cos(c.abs)
-          [r * Math.cos(b+c), r * Math.sin(b+c)]
-        end
-        x, y = cal.call
+      private
+      def create_pattern left, top, width, height, angle
+        width  = width * 0.5
+        height = height * 0.5
+        angle  = normalize_angle(angle)
+        args   = determine_args_based_on_angle(angle, height, left, top, width)
+        ::Swt::Pattern.new Shoes.display, args[0], args[1], args[2], args[3], color1.real, color2.real
+      end
+
+      def normalize_angle(angle)
+        angle = Math::PI * (angle/180.0)
+        angle % (Math::PI*2.0)
+      end
+
+      def determine_args_based_on_angle(angle, height, left, top, width)
+        x, y = calculate_x_and_y(angle, height, width)
         if 0 <= angle and angle < Math::PI*0.5
           args = [left+width+x, top+height-y, left+width-x, top+height+y]
         elsif Math::PI*0.5 <= angle and angle < Math::PI
@@ -48,7 +52,17 @@ class Shoes
         elsif Math::PI*1.5 <= angle and angle < Math::PI*2.0
           args = [left+width-y, top+height-x, left+width+y, top+height+x]
         end
-        ::Swt::Pattern.new Shoes.display, args[0], args[1], args[2], args[3], color1.real, color2.real
+        args
+      end
+
+      def calculate_x_and_y(angle, height, width)
+        length = Math.sqrt(width**2 + height**2)
+        b      = (height==0 and width==0) ? height/width : Math.atan(height/width)
+        c      = Math::PI*0.5 - angle - b
+        r      = length * Math.cos(c.abs)
+        x      = r * Math.cos(b+c)
+        y      = r * Math.sin(b+c)
+        return x, y
       end
     end
   end
