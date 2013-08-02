1771ca803fdb9f55157e2de5c739bc662a5cc723
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-02 21:48:05 +0200

    simple-app-dimensions.rb has never been a real sample.

--- samples/simple-app-dimensions.rb
+++
@@ -1,9 +0,0 @@
-Shoes.app width: 400, height: 300 do
-  stack margin: 20 do
-    @dimensions = title "width: #{width}, height: #{height} (default)"
-    para "Resize the window and click 'report' to show new dimensions"
-    button 'report' do
-      @dimensions.replace "width: #{width}, height: #{height}"
-    end
-  end
-end
