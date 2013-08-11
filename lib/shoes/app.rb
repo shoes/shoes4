8f46244887445906e20d10618d59ad56927a17a0
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-11 16:48:28 +0200

    a bit of DSL App#initialize refactoring

--- lib/shoes/app.rb
+++ lib/shoes/app.rb
@@ -37,32 +37,13 @@ class Shoes
     attr_writer   :width, :height
 
     def initialize(opts={}, &blk)
-      opts = DEFAULT_OPTIONS.merge(opts)
-
-      self.width      = opts[:width]
-      self.height     = opts[:height]
-      self.app_title  = opts[:title]
-      self.resizable  = opts[:resizable]
-      self.opts       = opts
-
-      @owner = opts[:owner]
-      @app = self
-      @style = default_styles
-      @contents, @unslotted_elements = [], []
-      @mouse_motion = []
-      @mouse_button, @mouse_pos = 0, [0, 0]
-      @mhcs = []
+      set_attributes_from_options(opts)
+      set_initial_attributes
       set_margin
 
       @gui = Shoes.configuration.backend::App.new @app
 
-      if blk
-        execution_blk = Proc.new do @app.instance_eval &blk end
-      elsif Shoes::URL.urls.keys.any? {|page| page.match '/'}
-        execution_blk = Proc.new do visit '/' end
-      else
-        execution_blk = nil
-      end
+      execution_blk = create_execution_block(blk)
 
       @top_slot = Flow.new self, self, { left: 0, top: 0, width: @width, height: @height}, &execution_blk
 
@@ -70,14 +51,6 @@ class Shoes
 
       Shoes.register self
       @gui.open
-    end
-
-    def add_console
-      keypress do |key|
-        if key == :"control_/"
-          ::Shoes::Logger.setup
-        end
-      end
     end
 
     def window(options={}, &block)
@@ -151,6 +124,52 @@ class Shoes
     end
 
     alias_method :fullscreen?, :fullscreen
+
+    private
+    def create_execution_block(blk)
+      if blk
+        execution_blk = Proc.new do
+          @app.instance_eval &blk
+        end
+      elsif Shoes::URL.urls.keys.any? { |page| page.match '/' }
+        execution_blk = Proc.new do
+          visit '/'
+        end
+      else
+        execution_blk = nil
+      end
+      execution_blk
+    end
+
+    def set_initial_attributes
+      @app                      = self
+      @style                    = default_styles
+      @contents                 = []
+      @unslotted_elements       = []
+      @mouse_motion             = []
+      @mouse_button, @mouse_pos = 0, [0, 0]
+      @mhcs                     = []
+    end
+
+    def set_attributes_from_options(opts)
+      opts = DEFAULT_OPTIONS.merge(opts)
+
+      self.width     = opts[:width]
+      self.height    = opts[:height]
+      self.app_title = opts[:title]
+      self.resizable = opts[:resizable]
+      self.opts      = opts
+
+      @owner = opts[:owner]
+    end
+
+    def add_console
+      keypress do |key|
+        if key == :"control_/"
+          ::Shoes::Logger.setup
+        end
+      end
+    end
 
   end
 end
