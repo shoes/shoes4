feddee3c8958cd5889921faeb905714b373ef37a
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-31 11:24:40 +0200

    image pattern duplication removal

--- lib/shoes/swt/image_pattern.rb
+++ lib/shoes/swt/image_pattern.rb
@@ -1,17 +1,17 @@
 class Shoes
   module Swt
     class ImagePattern
-      def initialize *args
-        @path = args.first.path
+      def initialize(dsl)
+        @dsl = dsl
       end
       
       def apply_as_fill(gc, left, top, width, height, angle = 0)
-        pattern = ::Swt::Pattern.new(Shoes.display, ::Swt::Image.new(Shoes.display, @path))
+        pattern = ::Swt::Pattern.new(Shoes.display, ::Swt::Image.new(Shoes.display, @dsl.path))
         gc.set_background_pattern pattern
       end
       
       def apply_as_stroke(gc, left, top, width, height, angle = 0)
-        pattern = ::Swt::Pattern.new(Shoes.display, ::Swt::Image.new(Shoes.display, @path))
+        pattern = ::Swt::Pattern.new(Shoes.display, ::Swt::Image.new(Shoes.display, @dsl.path))
         gc.set_foreground_pattern pattern
       end
     end
