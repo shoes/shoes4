f7138b78c70f7977b22409158a83e1356995c3f6
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-09-07 13:22:34 +0200

    duplicated data removal from oval

--- lib/shoes/oval.rb
+++ lib/shoes/oval.rb
@@ -1,27 +1,26 @@
 class Shoes
   class Oval
-    include Shoes::CommonMethods
-    include Shoes::Common::Fill
-    include Shoes::Common::Stroke
-    include Shoes::Common::Style
-    include Shoes::Common::Clickable
+    include CommonMethods
+    include Common::Fill
+    include Common::Stroke
+    include Common::Style
+    include Common::Clickable
+    include DimensionsDelegations
+
+    attr_reader :app, :dimensions, :angle
 
     def initialize(app, left, top, width, height, opts = {}, &blk)
       @app = app
-      @left = left
-      @top = top
-      @width = width
-      @height = height
+      @dimensions = Dimensions.new left, top, width, height
       @style = Shoes::Common::Fill::DEFAULTS.merge(Shoes::Common::Stroke::DEFAULTS).merge(opts)
       @style[:strokewidth] ||= @app.style[:strokewidth] || 1
+      @angle = opts[:angle]
+
       @app.unslotted_elements << self
 
-      # GUI
-      @gui = Shoes.backend_for(self, left, top, width, height, opts, &blk)
+      @gui = Shoes.backend_for(self, &blk)
 
       clickable_options(opts)
     end
-
-    attr_reader :app, :hidden
   end
 end
