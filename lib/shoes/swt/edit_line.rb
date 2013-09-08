b72f291865f9bb63d5df4ae034fff29ec2495b0a
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-09-08 17:09:26 +0200

    defaults are already set on the DSL side

--- lib/shoes/swt/edit_line.rb
+++ lib/shoes/swt/edit_line.rb
@@ -2,8 +2,6 @@ class Shoes
   module Swt
     class EditLine < InputBox
       def initialize(dsl, parent)
-        dsl.width ||= 200
-        dsl.height ||= 20
         styles = dsl.secret? ? ::Swt::SWT::SINGLE | ::Swt::SWT::BORDER | ::Swt::SWT::PASSWORD : ::Swt::SWT::SINGLE | ::Swt::SWT::BORDER
         super(dsl, parent, styles)
       end
