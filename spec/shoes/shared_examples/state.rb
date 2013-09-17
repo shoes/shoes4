b3b8c16d44871d00523f3196834bcf79ce167a65
Roger Jack <rjack@elegancetech.com>
2013-09-17 16:05:17 -0400

    Converted tabs to spaces
    Converted tabs to 2 spaces in new state.rb file.

--- spec/shoes/shared_examples/state.rb
+++ spec/shoes/shared_examples/state.rb
@@ -1,19 +1,19 @@
 shared_examples_for "object with state" do
-	let(:input_opts) { {:state => "disabled"} }
+  let(:input_opts) { {:state => "disabled"} }
 
-	it "should initialize" do
-	  subject.state.should == "disabled"
-	end
+  it "should initialize" do
+    subject.state.should == "disabled"
+  end
 
-	it "should enable" do
-	  subject.gui.should_receive(:enabled).with(true)
-	  subject.state = nil
-	  subject.state.should == nil
-	end
+  it "should enable" do
+    subject.gui.should_receive(:enabled).with(true)
+    subject.state = nil
+    subject.state.should == nil 
+  end
 
-	it "should disable" do
-	  subject.gui.should_receive(:enabled).with(false)
-	  subject.state = "disabled"
-	  subject.state.should == "disabled"
-	end
+  it "should disable" do
+    subject.gui.should_receive(:enabled).with(false)
+    subject.state = "disabled"
+    subject.state.should == "disabled"
+  end
 end
