db0f9f08d03b8bd76257fbcbeadc0796915f2e69
Roger Jack <rjack@elegancetech.com>
2013-10-03 14:08:49 -0400

    Modified shoes.bat to work from different directories
    Modified shoes.bat so that you can put your ruby file in any directory.
    Resolves issue #448.

--- bin/shoes.bat
+++ bin/shoes.bat
@@ -1,1 +1,8 @@
-call jruby --1.9 -e "$:<< 'lib'; require 'shoes'; require 'shoes/configuration'; Shoes.configuration.backend = :swt; require '%1' "
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+set bin_dir=%~dp0
+set root_dir=!bin_dir:~0,-4!
+set lib_dir=!root_dir!lib
+set command=jruby --1.9 -I "!lib_dir!" -e "require 'shoes'; require 'shoes/configuration'; Shoes.configuration.backend = :swt; require '%1' "
+call !command!
+endlocal
