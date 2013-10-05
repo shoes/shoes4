e580f8b49905130d5cdfbf7a451a01da69f1e54e
Roger Jack <rjack@elegancetech.com>
2013-10-05 12:15:28 -0400

    Simplified shoes.bat file
    Modified shoes.bat to use the ruby-shoes file.

--- bin/shoes.bat
+++ bin/shoes.bat
@@ -1,8 +1,6 @@
 @setlocal enableextensions enabledelayedexpansion
 @echo off
 set bin_dir=%~dp0
-set root_dir=!bin_dir:~0,-4!
-set lib_dir=!root_dir!lib
-set command=jruby --1.9 -I "!lib_dir!" -e "require 'shoes'; require 'shoes/configuration'; Shoes.configuration.backend = :swt; require '%1' "
+set command=jruby --1.9 !bin_dir!/ruby-shoes %1
 call !command!
 endlocal
