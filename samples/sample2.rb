a875883bdee4524590a5a30a6edb9cd3f1019f3c
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-10-07 23:59:02 +0200

    adjusted 2 samples so they are independent of where they are run from

--- samples/sample2.rb
+++ samples/sample2.rb
@@ -1,6 +1,6 @@
 Shoes.app title: 'Shoes 4 Logo Icon!', width: 310, height: 420 do
   stack do
-    path = 'static/shoes-icon.png'
+    path = File.join(Shoes::DIR, 'static/shoes-icon.png')
     image path
     flow do
       image path
