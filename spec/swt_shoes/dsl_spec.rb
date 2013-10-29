09c565262f8291c574557ca4185240981d26d1e0
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-10-30 00:26:54 +0100

    removed the repeated app.stubs

--- spec/swt_shoes/dsl_spec.rb
+++ spec/swt_shoes/dsl_spec.rb
@@ -22,16 +22,18 @@ describe "Basic Element Methods" do
 
   # Doubles for a Shoes::App
   let(:app) { DSLShoeLaces.new app_gui }
+  
+  before :each do
+    app.stub unslotted_elements: []
+  end
 
   describe "arc" do
     specify "creates a Shoes::Arc" do
       app_gui.should_receive(:add_paint_listener)
-      app.stub(:unslotted_elements){ [] }
       app.arc(1, 2, 101, 201, 11, 21).should be_an_instance_of(Shoes::Arc)
     end
 
     specify "raises an ArgumentError" do
-      app.stub(:unslotted_elements){ [] }
       lambda { app.arc(30) }.should raise_exception(ArgumentError)
     end
   end
@@ -39,12 +41,10 @@ describe "Basic Element Methods" do
   describe "rect" do
     specify "creates a Shoes::Rect" do
       app_gui.should_receive(:add_paint_listener)
-      app.stub(:unslotted_elements){ [] }
       app.rect(10, 20, 10, 20, angle: 45).should be_an_instance_of(Shoes::Rect)
     end
 
     specify "raises an ArgumentError" do
-      app.stub(:unslotted_elements){ [] }
       lambda { app.rect(30) }.should raise_exception(ArgumentError)
     end
   end
@@ -52,12 +52,10 @@ describe "Basic Element Methods" do
   describe "line" do
     specify "creates a Shoes::Line" do
       app_gui.should_receive(:add_paint_listener)
-      app.stub(:unslotted_elements){ [] }
       app.line(1, 2, 101, 201).should be_an_instance_of(Shoes::Line)
     end
 
     specify "raises an ArgumentError" do
-      app.stub(:unslotted_elements){ [] }
       lambda { app.line(30) }.should raise_error(ArgumentError)
     end
   end
@@ -65,12 +63,10 @@ describe "Basic Element Methods" do
   describe "oval" do
     specify "creates a Shoes::Oval" do
       app_gui.should_receive(:add_paint_listener)
-      app.stub(:unslotted_elements){ [] }
       app.oval(30, 20, 100, 200).should be_an_instance_of(Shoes::Oval)
     end
 
     specify "raises an ArgumentError" do
-      app.stub(:unslotted_elements){ [] }
       lambda { app.oval(10) }.should raise_error(ArgumentError)
     end
   end
@@ -78,12 +74,10 @@ describe "Basic Element Methods" do
   describe "star" do
     specify "creates a Shoes::Star" do
       app_gui.should_receive(:add_paint_listener)
-      app.stub(:unslotted_elements){ [] }
       app.star(30, 20).should be_an_instance_of(Shoes::Star)
     end
 
     specify "raises an ArgumentError" do
-      app.stub(:unslotted_elements){ [] }
       lambda { app.star(30) }.should raise_error(ArgumentError)
     end
   end
