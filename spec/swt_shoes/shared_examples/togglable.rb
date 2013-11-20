63c0b00bbe9c02e32ae8e394e8b0e98e5857e3e2
Jason R. Clark <jclark@newrelic.com>
2013-11-19 21:52:42 -0800

    Updating shared spec to RSpec 3 syntax

--- spec/swt_shoes/shared_examples/togglable.rb
+++ spec/swt_shoes/shared_examples/togglable.rb
@@ -1,12 +1,12 @@
 shared_examples_for "togglable" do
   it "triggers redrawing on the app" do
-    container.should_receive(:redraw)
+    expect(container).to receive(:redraw)
     subject.toggle
   end
 
   it "passes visibility to real object" do
     if subject.respond_to?(:gui) && subject.gui.respond_to?(:real)
-      subject.gui.real.should_receive(:set_visible)
+      expect(subject.gui.real).to receive(:set_visible)
       subject.toggle
     end
   end
