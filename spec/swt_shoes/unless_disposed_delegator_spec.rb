8f10fb0850663d9abd6cefada56db4ff44fc2b0f
Eric Watson <wasnotrice@gmail.com>
2013-12-22 15:27:55 -0600

    Add UnlessDisposedDelegator

---
+++ spec/swt_shoes/unless_disposed_delegator_spec.rb
@@ -0,0 +1,58 @@
+require 'delegate'
+
+class Shoes
+  module Swt
+    class UnlessDisposedDelegator < SimpleDelegator
+      def initialize(delegation_object)
+        super
+        @delegate_sd_null_obj = NullObject.new
+      end
+
+      def __getobj__
+        return NullObject.new if @delegate_sd_obj.disposed?
+        @delegate_sd_obj
+      end
+
+      class NullObject
+        def respond_to?(method)
+          true
+        end
+
+        def method_missing(method)
+        end
+      end
+    end
+  end
+end
+
+describe Shoes::Swt::UnlessDisposedDelegator do
+  let(:width) { 234 }
+  subject(:delegator) { Shoes::Swt::UnlessDisposedDelegator.new delegation_object }
+
+  context "when delegation object is NOT disposed" do
+    let(:delegation_object) { double("delegation object", width: width, disposed?: false) }
+
+    it "delegates method" do
+      delegator.width
+      expect(delegation_object).to have_received(:width)
+    end
+
+    it "returns the same result as the delegation object" do
+      expect(delegator.width).to eq(width)
+    end
+  end
+
+  context "when delegation object IS disposed" do
+    let(:delegation_object) { double("delegation object", width: width, disposed?: true) }
+
+    it "does not delegate method" do
+      delegator.width
+      expect(delegation_object).not_to have_received(:width)
+    end
+
+    it "returns nil" do
+      expect(delegator.width).to eq(nil)
+    end
+  end
+end
+
