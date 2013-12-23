95d368c71434b80388abce6ae3822daf7c9bf079
Eric Watson <wasnotrice@gmail.com>
2013-12-23 11:26:31 -0600

    Use a simpler delegation object

--- spec/swt_shoes/unless_disposed_delegator_spec.rb
+++ spec/swt_shoes/unless_disposed_delegator_spec.rb
@@ -2,33 +2,39 @@ require 'delegate'
 
 class Shoes
   module Swt
-    class UnlessDisposedDelegator < SimpleDelegator
-      def __getobj__
-        if @delegate_sd_obj.disposed?
-          @delegate_sd_null_obj ||= NullObject.new
-        else
-          @delegate_sd_obj
-        end
+    def with_disposed_protection(real)
+      WithDisposedProtection.new real
+    end
+
+    class WithDisposedProtection < BasicObject
+      def initialize(real)
+        @real = real
       end
 
-      class NullObject
-        def respond_to?(method)
-          true
-        end
+      def ==(other)
+        @real == other
+      end
 
-        def method_missing(method)
-        end
+      def !=(other)
+        @real != other
+      end
+
+      def method_missing(method, *args, &block)
+        @real.public_send method, *args, &block unless @real.disposed?
       end
     end
   end
 end
 
-describe Shoes::Swt::UnlessDisposedDelegator do
+
+describe Shoes::Swt::WithDisposedProtection do
+  include Shoes::Swt
+
   let(:width) { 234 }
-  subject(:delegator) { Shoes::Swt::UnlessDisposedDelegator.new delegation_object }
+  subject(:delegator) { with_disposed_protection delegation_object }
 
   context "when delegation object is NOT disposed" do
-    let(:delegation_object) { double("delegation object", width: width, disposed?: false) }
+    let(:delegation_object) { double("delegation object", width: width, dispose: nil, disposed?: false) }
 
     it "delegates method" do
       delegator.width
@@ -38,18 +44,28 @@ describe Shoes::Swt::UnlessDisposedDelegator do
     it "returns the same result as the delegation object" do
       expect(delegator.width).to eq(width)
     end
+
+    it "disposes delegation object" do
+      delegator.dispose
+      expect(delegation_object).to have_received(:dispose)
+    end
   end
 
   context "when delegation object IS disposed" do
-    let(:delegation_object) { double("delegation object", width: width, disposed?: true) }
+    let(:delegation_object) { double("delegation object", width: width, dispose: nil, disposed?: true) }
 
-    it "does not delegate method" do
+    it "does NOT delegate method" do
       delegator.width
       expect(delegation_object).not_to have_received(:width)
     end
 
     it "returns nil" do
       expect(delegator.width).to eq(nil)
+    end
+
+    it "does NOT dispose delegation object" do
+      delegator.dispose
+      expect(delegation_object).not_to have_received(:dispose)
     end
   end
 end
