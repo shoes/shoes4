0f8d9af5ff5954857e1d49f49c9429a79ab508a5
Eric Watson <wasnotrice@gmail.com>
2014-01-01 13:19:32 -0600

    Fix style specs for app

--- spec/shoes/app_spec.rb
+++ spec/shoes/app_spec.rb
@@ -97,58 +97,64 @@ describe Shoes::App do
     end
   end
 
-  # This behavior is different from Red Shoes. Red Shoes doesn't expose
-  # the style hash on Shoes::App.
   describe "style" do
-    subject { Shoes::App.new }
-    it_behaves_like "object with style"
-  end
-
-  describe "strokewidth" do
-    it "defaults to 1" do
-      subject.style[:strokewidth].should eq(1)
-    end
-
-    it "passes default to objects" do
-      subject.line(0, 100, 100, 0).style[:strokewidth].should eq(1)
-    end
-
-    it "passes new values to objects" do
-      subject.strokewidth 10
-      subject.line(0, 100, 100, 0).style[:strokewidth].should eq(10)
-    end
-  end
-
-  describe "stroke" do
     let(:black) { Shoes::COLORS[:black] }
     let(:goldenrod) { Shoes::COLORS[:goldenrod] }
-    it "defaults to black" do
-      subject.style[:stroke].should eq(black)
+    let(:defaults) { {stroke: black, strokewidth: 1} }
+
+    it "sets defaults" do
+      expect(app.style).to eq(defaults)
     end
 
-    it "passes default to objects" do
-      subject.oval(100, 100, 100).style[:stroke].should eq(black)
+    it "merges new styles with existing styles" do
+      new_styles = { strokewidth: 4 }
+      app.style new_styles
+      expect(app.style).to eq(defaults.merge(new_styles))
     end
 
-    it "passes new value to objects" do
-      subject.stroke goldenrod
-      subject.oval(100, 100, 100).style[:stroke].should eq(goldenrod)
+    describe "strokewidth" do
+      it "defaults to 1" do
+        subject.style[:strokewidth].should eq(1)
+      end
+
+      it "passes default to objects" do
+        subject.line(0, 100, 100, 0).style[:strokewidth].should eq(1)
+      end
+
+      it "passes new values to objects" do
+        subject.strokewidth 10
+        subject.line(0, 100, 100, 0).style[:strokewidth].should eq(10)
+      end
+    end
+
+    describe "stroke" do
+      it "defaults to black" do
+        subject.style[:stroke].should eq(black)
+      end
+
+      it "passes default to objects" do
+        subject.oval(100, 100, 100).style[:stroke].should eq(black)
+      end
+
+      it "passes new value to objects" do
+        subject.stroke goldenrod
+        subject.oval(100, 100, 100).style[:stroke].should eq(goldenrod)
+      end
+    end
+
+    describe "default styles" do
+      it "is independent among Shoes::App instances" do
+        app1 = Shoes::App.new
+        app2 = Shoes::App.new
+
+        app1.strokewidth 10
+        app1.line(0, 100, 100, 0).style[:strokewidth].should == 10
+
+        # .. but does not affect app2
+        app2.line(0, 100, 100, 0).style[:strokewidth].should_not == 10
+      end
     end
   end
-
-  describe "default styles" do
-    it "is independent among Shoes::App instances" do
-      app1 = Shoes::App.new
-      app2 = Shoes::App.new
-
-      app1.strokewidth 10
-      app1.line(0, 100, 100, 0).style[:strokewidth].should == 10
-
-      # .. but does not affect app2
-      app2.line(0, 100, 100, 0).style[:strokewidth].should_not == 10
-    end
-  end
-
   describe "clipboard" do
     it "gets clipboard" do
       subject.gui.should_receive(:clipboard)
