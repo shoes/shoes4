721904fc46f69430b3e9b048f925b83a4de9b0ef
Eric Watson <wasnotrice@gmail.com>
2014-01-01 13:19:55 -0600

    Update style specs to expect syntax

--- spec/shoes/shared_examples/style.rb
+++ spec/shoes/shared_examples/style.rb
@@ -3,16 +3,16 @@ shared_examples_for "object with style" do
     old_style = subject.style
     subject.style :left => 100
     subject.style :top => 50
-    subject.style.should eq(old_style.merge(:left => 100, :top => 50))
+    expect(subject.style).to eq(old_style.merge(:left => 100, :top => 50))
   end
 
   it 'only calls change_style when the style is changed' do
-    subject.should_receive :change_style
     subject.style :left => 50
+    expect(subject).to have_received(:change_style)
   end
 
-  it 'does nto call change_style when style is called without args' do
-    subject.should_not_receive :change_style
+  it 'does not call change_style when style is called without args' do
     subject.style
+    expect(subject).not_to have_received(:change_style)
   end
 end
