3819a5d5d6cc792f845d47760c3e12605d3f4c5d
Eric Watson <wasnotrice@gmail.com>
2014-01-01 15:55:33 -0600

    Rect specs: use shared context, expect syntax

--- spec/shoes/shared_examples/style.rb
+++ spec/shoes/shared_examples/style.rb
@@ -6,13 +6,19 @@ shared_examples_for "object with style" do
     expect(subject.style).to eq(old_style.merge(:left => 100, :top => 50))
   end
 
-  it 'only calls change_style when the style is changed' do
-    subject.style :left => 50
-    expect(subject).to have_received(:change_style)
-  end
+  describe 'changing style' do
+    before do
+      subject.stub(:change_style)
+    end
 
-  it 'does not call change_style when style is called without args' do
-    subject.style
-    expect(subject).not_to have_received(:change_style)
+    it 'calls change_style when the style is changed' do
+      subject.style :left => 50
+      expect(subject).to have_received(:change_style)
+    end
+
+    it 'does not call change_style when style is called without args' do
+      subject.style
+      expect(subject).not_to have_received(:change_style)
+    end
   end
 end
