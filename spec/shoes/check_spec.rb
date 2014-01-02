ee32a6ac71d88ebb0bed9b757a3dcbaeff55ce2f
Eric Watson <wasnotrice@gmail.com>
2014-01-01 21:32:59 -0600

    Check specs: update to use shared context
    Also, consolidate check specs in a single file

--- spec/shoes/check_spec.rb
+++ spec/shoes/check_spec.rb
@@ -1,12 +1,31 @@
 require 'shoes/spec_helper'
 
 describe Shoes::Check do
+  include_context "dsl app"
+
   subject { Shoes::Check.new(app, parent, input_opts, input_block) }
-  let(:input_opts) { Hash.new }
-  let(:input_block) { Proc.new {} }
-  let(:app) { Shoes::App.new }
-  let(:parent) { Shoes::Flow.new app, app }
 
   it_behaves_like "checkable"
   it_behaves_like "object with state"
+
+  describe "dimensions" do
+    let(:left) { 10 }
+    let(:top) { 20 }
+    let(:width) { 100 }
+    let(:height) { 200 }
+    let(:input_opts){ {left: left, top: top, width: width, height: height} }
+    subject { Shoes::Check.new(app, parent, input_opts) }
+
+    it_behaves_like "object with dimensions"
+
+    describe "takes relative dimensions from parent" do
+      subject { Shoes::Check.new(app, parent, relative_opts) }
+      it_behaves_like "object with relative dimensions"
+    end
+
+    describe "negative dimensions" do
+      subject { Shoes::Check.new(app, parent, negative_opts) }
+      it_behaves_like "object with negative dimensions"
+    end
+  end
 end
