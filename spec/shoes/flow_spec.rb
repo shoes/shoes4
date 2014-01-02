f5e157ae5646691271fd8cc8e2da25673847d27a
Eric Watson <wasnotrice@gmail.com>
2014-01-01 21:06:55 -0600

    Flow specs: use shared context, expect syntax

--- spec/shoes/flow_spec.rb
+++ spec/shoes/flow_spec.rb
@@ -2,43 +2,40 @@ require 'shoes/spec_helper'
 require_relative 'helpers/fake_element'
 
 describe Shoes::Flow do
-  let(:app) { parent }
-  let(:parent) { Shoes::App.new }
-  let(:input_block) { Proc.new {} }
-  let(:opts) { Hash.new }
+  include_context "dsl app"
 
-  subject { Shoes::Flow.new(app, parent, opts, &input_block) }
+  subject(:flow) { Shoes::Flow.new(app, parent, input_opts, &input_block) }
 
   it_behaves_like "clickable object"
   it_behaves_like "hover and leave events"
   it_behaves_like "Slot"
 
   describe "initialize" do
-    let(:opts) { {:width => 131, :height => 137} }
+    let(:input_opts) { {:width => 131, :height => 137} }
     it "sets accessors" do
-      subject.parent.should == parent
-      subject.width.should == 131
-      subject.height.should == 137
-      subject.blk.should == input_block
+      expect(flow.parent).to eq(parent)
+      expect(flow.width).to eq(131)
+      expect(flow.height).to eq(137)
+      expect(flow.blk).to eq(input_block)
     end
 
     describe 'margins' do
-      let(:opts){{margin: 143}}
+      let(:input_opts) { {margin: 143} }
       it 'sets the margins' do
-        subject.margin.should == [143, 143, 143, 143]
+        expect(flow.margin).to eq([143, 143, 143, 143])
       end
     end
   end
 
   it "sets default values" do
     f = Shoes::Flow.new(app, parent)
-    f.width.should == app.width
-    f.height.should == 0
+    expect(f.width).to eq(app.width)
+    expect(f.height).to eq(0)
   end
 
   it "clears with an optional block" do
-    subject.should_receive(:clear).with(&input_block)
-    subject.clear &input_block
+    expect(flow).to receive(:clear).with(&input_block)
+    flow.clear &input_block
   end
 
   describe "positioning" do
@@ -47,15 +44,15 @@ describe Shoes::Flow do
 
     shared_examples_for 'positioning in the same line' do
       it 'positions an element in the same row as long as they fit' do
-        element2.absolute_top.should eq subject.absolute_top
+        expect(element2.absolute_top).to eq(flow.absolute_top)
       end
 
       it 'positions it next to the other element as long as it fits' do
-        element2.absolute_left.should eq element.absolute_right + 1
+        expect(element2.absolute_left).to eq(element.absolute_right + 1)
       end
 
       it 'has a slot height of the maximum value of the 2 elements' do
-        subject.height.should eq [element.height, element2.height].max
+        expect(flow.height).to eq([element.height, element2.height].max)
       end
     end
 
@@ -66,31 +63,30 @@ describe Shoes::Flow do
       it_behaves_like 'positioning in the same line'
 
       describe 'exact fit' do
-        let(:opts) {{width: element.width + element2.width}}
+        let(:input_opts) {{width: element.width + element2.width}}
         it_behaves_like 'positioning in the same line'
       end
 
     end
 
     describe 'when the elements dont fit next to each other' do
-      let(:opts){ {width: element.width + 10} }
+      let(:input_opts){ {width: element.width + 10} }
       it_behaves_like 'arranges elements underneath each other'
     end
 
     describe 'elements dont fit next to each other and set small height' do
-      let(:opts){ {width: element.width + 10, height: element.height + 10} }
+      let(:input_opts) { {width: element.width + 10, height: element.height + 10} }
       it_behaves_like 'set height and contents alignment'
     end
 
     describe 'elements dont fit next to each other and set big height' do
-      let(:opts){ {width: element.width + 10, height: 1000} }
+      let(:input_opts){ {width: element.width + 10, height: 1000} }
       it_behaves_like 'set height and contents alignment'
     end
 
     describe 'with margins and two elements not fitting next to each other' do
-      let(:opts){{width: element.width + 10, margin: 27}}
+      let(:input_opts){{width: element.width + 10, margin: 27}}
       it_behaves_like 'taking care of margin'
     end
-
   end
 end
