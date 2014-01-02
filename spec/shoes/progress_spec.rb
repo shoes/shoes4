2b5f52652e36f9b097a77e4f35255025d14e33a7
Eric Watson <wasnotrice@gmail.com>
2014-01-01 22:29:46 -0600

    Progress specs: use shared context, expect syntax

--- spec/shoes/progress_spec.rb
+++ spec/shoes/progress_spec.rb
@@ -1,17 +1,15 @@
 require 'shoes/spec_helper'
 
 describe Shoes::Progress do
+  include_context "dsl app"
+  let(:input_opts) { { left: left, top: top, width: width, height: height } }
+
   let(:left) { 10 }
   let(:top) { 20 }
   let(:width) { 100 }
   let(:height) { 200 }
 
-  let(:input_block) { Proc.new {} }
-  let(:input_opts) { { left: left, top: top, width: width, height: height } }
-  let(:app) { Shoes::App.new }
-  let(:parent) { Shoes::Flow.new app, app}
-
-  subject { Shoes::Progress.new(app, parent, input_opts, input_block) }
+  subject(:progress) { Shoes::Progress.new(app, parent, input_opts, input_block) }
 
   it { should respond_to :fraction }
   it { should respond_to :fraction= }
@@ -20,13 +18,13 @@ describe Shoes::Progress do
 
   context "setting fraction" do
     it "sets on gui" do
-      subject.gui.should_receive(:fraction=).with(0.5)
-      subject.fraction = 0.5
+      expect(progress.gui).to receive(:fraction=).with(0.5)
+      progress.fraction = 0.5
     end
 
     it "sets on self" do
-      subject.fraction = 0.5
-      subject.fraction.should eq 0.5
+      progress.fraction = 0.5
+      expect(progress.fraction).to eq(0.5)
     end
   end
 
