363d5abe3c2eadb182ddadd55f120e11ea63274c
Eric Watson <wasnotrice@gmail.com>
2014-01-01 19:03:04 -0600

    Slot specs: use shared context, expect syntax

--- spec/shoes/slot_spec.rb
+++ spec/shoes/slot_spec.rb
@@ -1,13 +1,14 @@
 require 'shoes/spec_helper'
 
 describe Shoes::Slot do
+  include_context "dsl app"
+  let(:parent) { app }
+
   let(:left) { 44 }
   let(:top) { 66 }
   let(:width) { 111 }
   let(:height) { 333 }
-  let(:app) { Shoes::App.new }
-  let(:parent) { app }
-  subject { Shoes::Slot.new(app, parent, left: left, top: top, width: width, height: height) }
+  subject(:slot) { Shoes::Slot.new(app, parent, left: left, top: top, width: width, height: height) }
 
   it_behaves_like "object with dimensions"
 
@@ -24,15 +25,15 @@ describe Shoes::Slot do
   describe 'clearing' do
     before :each do
       10.times do
-        Shoes::TextBlock.new app, subject, ['text'], 20
+        Shoes::TextBlock.new app, slot, ['text'], 20
       end
     end
 
     it 'sends remove to all children' do
-      subject.contents.each do
-        |element| element.should_receive(:remove).and_call_original
+      subject.contents.each do |element|
+        expect(element).to receive(:remove).and_call_original
       end
-      subject.clear
+      slot.clear
     end
   end
 end
