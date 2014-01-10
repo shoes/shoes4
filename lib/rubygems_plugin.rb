2035ad955a444e448cdd0c801f865d4bf984532f
Jason R. Clark <jclark@newrelic.com>
2014-01-10 18:00:32 -0500

    Ensure we're uninstalling shoes from Rubygems plugin

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,9 +2,12 @@ require 'rubygems'
 require 'fileutils'
 
 # See ext/Rakefile for explanation of why this cleanup is our responsibility.
-Gem.post_uninstall do
-  # Decide if user uninstalled executables based on our actual Ruby script
-  unless File.exists?(File.join(Gem.bindir, "ruby-shoes"))
+# Decide if user uninstalled executables based on our actual Ruby script
+Gem.post_uninstall do |gem|
+  uninstalling_shoes = gem.spec.name == "shoes"
+  missing_executable = !File.exists?(File.join(Gem.bindir, "ruby-shoes"))
+
+  if uninstalling_shoes && missing_executable
     puts "Removing shoes"
     FileUtils.rm(File.join(Gem.bindir, "shoes"))
   end
