999e31b3cf3d9c2010c550fd96affbbf0f29d18a
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-01-20 20:36:22 +0100

    Wrap sample45 titles in a stack so it does not look weird
    * [ci skip]

--- samples/sample45.rb
+++ samples/sample45.rb
@@ -1,8 +1,10 @@
 Shoes.app height: 200 do
   font File.join(Shoes::DIR, 'fonts/Coolvetica.ttf')
-  title 'Good Question'
-  title 'Good Question', font: 'Coolvetica'
-  title 'Good Question', font: 'Lucida console'
+  stack do
+    title 'Good Question'
+    title 'Good Question', font: 'Coolvetica'
+    title 'Good Question', font: 'Lucida console'
+  end
   para 'Arial (default)', left: 400, top: 25
   para 'Coolvetica', left: 400, top: 70
   para 'Lucida Console', left: 400, top: 120
