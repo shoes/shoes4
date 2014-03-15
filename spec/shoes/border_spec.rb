c57fc72ef354586db8b35f29b582e6b34b7a076e
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-03-15 23:52:51 +0100

    Apparently it also needs margin_right/bottom sometimes for openjdk6
    * build reference https://travis-ci.org/shoes/shoes4/builds/20853575
    * see previous commit for more info

--- spec/shoes/border_spec.rb
+++ spec/shoes/border_spec.rb
@@ -15,7 +15,8 @@ describe Shoes::Border do
   let(:parent) { double 'parent', absolute_left: left, absolute_top: top,
                  width: width, height: height, add_child: true,
                  element_width: width, element_height: height,
-                 margin_left: 0, margin_top: 0}
+                 margin_left: 0, margin_top: 0, margin_right: 0,
+                 margin_bottom: 0}
   let(:opts){ {left: left, top: top, width: width, height: height} }
 
   let(:left) { 10 }
