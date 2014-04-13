d9448cd1ded89e94a555c114c6664d99a9634ac5
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-04-13 21:18:21 +0200

    Could not find a usage of this method - revert if necessary

--- lib/shoes/swt/rect_painter.rb
+++ lib/shoes/swt/rect_painter.rb
@@ -1,27 +1,6 @@
 class Shoes
   module Swt
     class RectPainter < Common::Painter
-      def path
-        path = ::Swt::Path.new(display)
-        # Windows won't do the rounded rectangle path with a corner radius of 0
-        if @obj.corners.zero?
-          path.add_rectangle(@obj.element_left, @obj.element_top,
-                             @obj.element_width, @obj.element_height)
-        else
-          diameter = @obj.corners * 2
-          path.add_arc(@obj.element_left, @obj.element_top,
-                       diameter, diameter, 180, -90)
-          path.add_arc(@obj.element_right - diameter, @obj.element_top,
-                       diameter, diameter, 90, -90)
-          path.add_arc(@obj.element_right - diameter,
-                       @obj.element_bottom - diameter, diameter, diameter, 0, -90)
-          path.add_arc(@obj.element_left, @obj.element_bottom - diameter,
-                       diameter, diameter, -90, -90)
-          path.line_to(@obj.element_left, @obj.element_top + @obj.corners)
-        end
-        path
-      end
-
       def fill(graphics_context)
         graphics_context.fill_round_rectangle(@obj.element_left,
                                               @obj.element_top,
