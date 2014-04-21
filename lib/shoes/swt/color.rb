f399690cc38f210802efaacd1ff24dbae9848d48
Eric Watson <wasnotrice@gmail.com>
2014-04-20 22:07:33 -0500

    Revert "Experiment with disposing resources in finalizers"
    This reverts commit 10690081b920f8526e585549c4a38998daccd9a6.

--- lib/shoes/swt/color.rb
+++ lib/shoes/swt/color.rb
@@ -11,7 +11,6 @@ class Shoes
       def initialize(color)
         @dsl = color
         @real = ::Swt::Graphics::Color.new(Shoes.display, @dsl.red, @dsl.green, @dsl.blue)
-        mark_for_disposal(@real)
       end
 
       attr_reader :dsl
--- lib/shoes/swt/disposed_protection.rb
+++ lib/shoes/swt/disposed_protection.rb
@@ -1,25 +1,6 @@
 class Shoes
   module Swt
     module DisposedProtection
-      def mark_for_disposal(resource_objects)
-        ObjectSpace.define_finalizer(self, self.class.finalize(resource_objects))
-      end
-
-      def self.included(mod)
-        mod.extend ClassMethods
-      end
-
-      module ClassMethods
-        def finalize(resource_objects)
-          Proc.new do
-            Array(resource_objects).each do |resource|
-              puts "#{Time.now.strftime("[%H:%M:%S:%L]")} Disposing #{resource.inspect}"
-              resource.dispose unless resource.disposed?
-            end
-          end
-        end
-      end
-
       def real
         return NullObject.new(@real) if @real.disposed?
         @real
