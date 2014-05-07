cca311c5389331fb2cd14cd5c9e9f055c7485709
Eric Watson <wasnotrice@gmail.com>
2014-05-06 22:57:54 -0500

    Track all Swt Resource subclasses, report totals
    - tracks all known Swt::Graphics::Resource subclasses
    - in addition to the count => backtrace report, also includes a
      class => total report
    - adds tracking for resources that were disposed without being tracked.
      These seem to be all Swt::Graphics::GC objects. Probably these are the
      objects created by Swt (i.e. we didn't call .new on them), that we are
      not disposing ourselves.

--- lib/shoes/swt/leak_hunter.rb
+++ lib/shoes/swt/leak_hunter.rb
@@ -2,31 +2,53 @@ require 'swt'
 require 'shoes/swt'
 
 $callers = {}
+$untracked_disposals = {}
 
-classes = [
-  ::Swt::Graphics::Color,
-  ::Swt::Graphics::Font,
-  ::Swt::TextLayout,
-]
+-> do
+  # All known subclasses of Swt::Graphics::Resource
+  # see http://help.eclipse.org/helios/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Freference%2Fapi%2Forg%2Feclipse%2Fswt%2Fpackage-summary.html
+  classes = [
+    ::Swt::Graphics::Color,
+    ::Swt::Graphics::Cursor,
+    ::Swt::Graphics::Font,
+    ::Swt::Graphics::GC,
+    ::Swt::Graphics::Image,
+    ::Swt::Graphics::Path,
+    ::Swt::Graphics::Pattern,
+    ::Swt::Graphics::Region,
+    ::Swt::Graphics::TextLayout,
+    ::Swt::Graphics::Transform,
+  ]
 
-classes.each do |klass|
-  klass.class_eval do
-    define_singleton_method(:new) do |*args|
-      key = "[#{self.name}]\n" + caller.join("\n\t")
-      $callers[key] ||= 0
-      $callers[key] += 1
+  create_key = ->(klass, backtrace) do
+    "[#{klass}]\n" + backtrace.join("\n\t")
+  end
 
-      result = super(*args)
-      result.instance_variable_set(:@__shoes_creation_backtrace, key)
-      result
-    end
+  classes.each do |klass|
+    klass.class_eval do
+      define_singleton_method(:new) do |*args|
+        key = create_key.call(self.name, caller)
+        $callers[key] ||= 0
+        $callers[key] += 1
 
-    define_method(:dispose) do |*args|
-      $callers[@__shoes_creation_backtrace] -= 1
-      super(*args)
+        result = super(*args)
+        result.instance_variable_set(:@__shoes_creation_backtrace, key)
+        result
+      end
+
+      define_method(:dispose) do |*args|
+        if $callers[@__shoes_creation_backtrace]
+          $callers[@__shoes_creation_backtrace] -= 1
+        else
+          key = create_key.call(self.class.name, caller)
+          $untracked_disposals[key] ||= 0
+          $untracked_disposals[key] += 1
+        end
+        super(*args)
+      end
     end
   end
-end
+end.call
 
 at_exit do
   $callers.each do |k, v|
@@ -35,4 +57,21 @@ at_exit do
       puts
     end
   end
+
+  totals = ->(hash) do
+    hash.inject({}) do |memo, (k, v)|
+      klass = k.split("\n").first
+      memo[klass] ||= 0
+      memo[klass] += v
+      memo
+    end.map do |k, v|
+      v == 0 ? nil : " %-42s %5d" % [k, v]
+    end.compact.join("\n")
+  end
+
+  puts "Totals:"
+  puts totals.call($callers)
+  puts
+  puts "Untracked disposals:"
+  puts totals.call($untracked_disposals)
 end
