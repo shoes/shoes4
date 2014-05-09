936c83cb5fc89d5548ff00f10f4fb46be978a943
Jason R. Clark <jclark@newrelic.com>
2014-05-09 17:51:42 +0100

    Adding ColorFactory for tracking SWT color resources

--- lib/shoes/swt.rb
+++ lib/shoes/swt.rb
@@ -52,6 +52,7 @@ Shoes.configuration.backend = :swt
 
 require 'shoes/swt/disposed_protection'
 require 'shoes/swt/color'
+require 'shoes/swt/color_factory'
 require 'shoes/swt/common/child'
 require 'shoes/swt/common/clear'
 require 'shoes/swt/common/clickable'
