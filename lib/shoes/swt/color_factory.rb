96fe42f8678e1b8fd5d64b34848c3529c63f34f6
Jason R. Clark <jclark@newrelic.com>
2014-05-09 19:44:58 +0100

    Fix for letting Gradients through ColorFactory

--- lib/shoes/swt/color_factory.rb
+++ lib/shoes/swt/color_factory.rb
@@ -1,22 +1,31 @@
 class Shoes
   module Swt
+    # ColorFactory is used for tracking SWT-backed resources related to colors
+    # that potentially need to get disposed. It can additionally take gradients
+    # and maybe other values, though, so it can't assume values will be
+    # disposable.
+    #
+    # Additionally this factory does caching against the DSL element so we
+    # don't needlessly generate things like OS colors over and over again.
     class ColorFactory
       def initialize
-        @swt_colors = {}
+        @swt_elements = {}
       end
 
       def dispose
-        @swt_colors.each_value(&:dispose)
-        @swt_colors.clear
+        @swt_elements.each_value do |swt_element|
+          swt_element.dispose if swt_element.respond_to?(:dispose)
+        end
+        @swt_elements.clear
       end
 
-      def create(color)
-        return nil if color.nil?
-        return @swt_colors[color] if @swt_colors.include?(color)
+      def create(element)
+        return nil if element.nil?
+        return @swt_elements[element] if @swt_elements.include?(element)
 
-        swt_color = ::Shoes.configuration.backend_for(color)
-        @swt_colors[color] = swt_color
-        swt_color
+        swt_element = ::Shoes.configuration.backend_for(element)
+        @swt_elements[element] = swt_element
+        swt_element
       end
     end
   end
