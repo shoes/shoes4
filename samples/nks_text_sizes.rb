597cc82557249d48ec61f88fd96fb7c95fe4b800
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-05-17 17:32:50 +0200

    Import 2 nks examples to the samples/ dir
    * #700
    * decided to keep the other 2 breadsticks samples for now as they
      are both modified with some other code and not the original
    * fixed the nks_text_size up a bit -> e.g. the code in the book
      did not have a stack but it looked like using a stack
    * [ci skip]

---
+++ samples/nks_text_sizes.rb
@@ -0,0 +1,10 @@
+Shoes.app width: 150, height: 200 do
+  stack do
+    title "Title"
+    subtitle "Subtitle"
+    tagline "Tagline"
+    caption "Caption"
+    para "Para"
+    inscription "Inscription"
+  end
+end
