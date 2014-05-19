4c746d6218cf50cd908df6dea2d97170f1e7f4ed
Eric Watson <wasnotrice@gmail.com>
2014-05-19 10:43:56 -0500

    Add basic 'rake changelog' command
    Searches git log for marked commits. Results organized by
    categories 'feature', 'improvement', 'bugfix'
    
    Prints results to STDOUT

---
+++ tasks/changelog.rb
@@ -0,0 +1,37 @@
+class Changelog
+  def generate(categories = nil)
+    categories ||= [
+      { token: 'feature', heading: 'New features'},
+      { token: 'improvement', heading: 'Improvements'},
+      { token: 'bugfix', heading: 'Bug Fixes'}
+    ]
+
+    last_release = `git describe #{`git rev-list --tags --max-count=1`}`
+    grep_placeholder = '{TOKEN}'
+    log_command_template = "git log --grep '#{grep_placeholder}' --format='* %s (%an) [%h]' --since #{last_release}"
+
+    changes = categories.inject('') do |list, category|
+      grep_pattern = "[Cc]hangelog: #{category[:token]}"
+      log_command = log_command_template.gsub(grep_placeholder, grep_pattern)
+      list << heading(category[:heading])
+      list << `#{log_command}`
+      raise "Error scanning git log. Using <#{log_command}" unless $?.success?
+      list
+    end
+
+    # TODO: Anything marked 'Changelog' without a parameter
+    # changes << log_command.gsub(grep_placeholder, "[Cc]hangelog")
+
+    changes
+  end
+
+  def heading(title, underline_char = '-')
+    underline = underline_char * title.length
+    "#{title}\n#{underline}\n\n"
+  end
+end
+
+desc "Generate changelog entries since last release"
+task :changelog do
+  puts Changelog.new.generate
+end
