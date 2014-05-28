1f8b5ac51fdccb53611a4b82b06ef09a9ab1865c
Eric Watson <wasnotrice@gmail.com>
2014-05-28 08:43:43 -0500

    Add install/release gem tasks for sub-gems

--- tasks/gem.rb
+++ tasks/gem.rb
@@ -1,14 +1,26 @@
 require 'bundler/gem_helper'
 
-desc 'Build all gems'
-task 'build:all'
+# Defer installing Bundler gem tasks until runtime, so we can install them
+# for a particular gem. Still create tasks that will show up in `rake --tasks`
 ['shoes', 'shoes-dsl', 'shoes-swt'].each do |lib|
-  desc "Build the #{lib} gem"
-  task "build:#{lib}" do
+  task "install_gem_tasks:#{lib}" do
     Bundler::GemHelper.install_tasks :name => lib
-    Rake::Task['build'].invoke
   end
 
-  task 'build:all' => "build:#{lib}"
+  ['build', 'install', 'release'].each do |action|
+    # Define a placeholder task so we can name it as a dependency. The body
+    # of this task will be defined by Bundler at runtime.
+    task action
+
+    desc "#{action.capitalize} the #{lib} gem"
+    task "#{action}:#{lib}" => ["install_gem_tasks:#{lib}", action]
+
+    unless action == 'release'
+      desc "#{action.capitalize} all gems"
+      task "#{action}:all"
+
+      task "#{action}:all" => "#{action}:#{lib}"
+    end
+  end
 end
 
