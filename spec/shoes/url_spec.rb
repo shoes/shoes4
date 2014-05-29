dbcad1164dc45aff5f9d6abfcbb8123f8fc1f4b8
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-05-29 20:18:33 +0200

    include RSpec::Matchers to make DSL available in URL class
    * gets rid of $global -jay
    * thanks @myronmarston for pointing this out! =)

--- spec/shoes/url_spec.rb
+++ spec/shoes/url_spec.rb
@@ -3,6 +3,8 @@ require 'shoes/spec_helper'
 describe 'Shoes.url' do
   let(:klazz) do
     Class.new(Shoes) do
+      include RSpec::Matchers
+
       url '/', :index
       url '/path', :path
       url '/number/(\d+)', :number
@@ -23,7 +25,7 @@ describe 'Shoes.url' do
       end
 
       def visit_me
-        $location = location # I really couldn't come up with a better way
+        expect(location).to eq '/visit_me'
       end
 
       def some_method
@@ -57,7 +59,6 @@ describe 'Shoes.url' do
 
   it 'has a location method that returns the current URL' do
     Shoes.app do visit('/visit_me') end
-    expect($location).to eq '/visit_me'
   end
 
   it 'instances report class as klazz (regression, do not ask...)' do
