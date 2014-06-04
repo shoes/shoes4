be703d2c71bae05e2db8d768f843266c02b7afe3
KC Erb <iamkcerb@gmail.com>
2014-06-03 20:53:54 -0600

    updated it_styles_with

--- spec/shoes/spec_helper.rb
+++ spec/shoes/spec_helper.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
 def it_styles_with(*styles)
-  #unpack styles 
+  supported_styles = unpack_styles(styles)
+  spec_styles(supported_styles)
+end
+
+def unpack_styles(*styles)
   supported_styles = []
-    styles.each do |style|
-      if Shoes::Common::Style::STYLE_GROUPS[style]
-        Shoes::Common::Style::STYLE_GROUPS[style].each{|style| supported_styles << style}
-      else
-        supported_styles << style
-      end
+  styles.each do |style|
+    if Shoes::Common::Style::STYLE_GROUPS[style]
+      Shoes::Common::Style::STYLE_GROUPS[style].each{|style| supported_styles << style}
+    else
+      @supported_styles << style
     end
-    
-    #check for style behavior
-    supported_styles.each do |style|
-      it_behaves_like "object that styles with #{style}"
-    end
+  end
+  supported_styles
+end
+
+def spec_styles(supported_styles)
+  supported_styles.each do |style|
+    it_behaves_like "object that styles with #{style}"
+  end
 end
