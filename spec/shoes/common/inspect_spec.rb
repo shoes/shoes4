2cb2c7078347510aff799b062a11fa494db379a4
Eric Watson <wasnotrice@gmail.com>
2014-06-06 15:10:44 -0500

    Use a dedicated object for testing

--- spec/shoes/common/inspect_spec.rb
+++ spec/shoes/common/inspect_spec.rb
@@ -1,16 +1,21 @@
 require "spec_helper"
 
-describe "Shoes::Common::Inspect" do
-  include_context "dsl app"
-  describe "when included" do
-    subject(:rect) { Shoes::Rect.new(app, parent, 0, 0, 100, 200) }
+class Shoes
+  class InspectableObject
+    include Shoes::Common::Inspect
+  end
+end
 
-    it "gives a #to_s like (Shoes:Rect)" do
-      expect(rect.to_s).to eq("(Shoes:Rect)")
+describe "Shoes::Common::Inspect" do
+  describe "when included" do
+    subject(:object) { Shoes::InspectableObject.new }
+
+    it "gives a #to_s like (Shoes:Klass)" do
+      expect(object.to_s).to eq("(Shoes:InspectableObject)")
     end
 
-    it "gives an #inspect like (Shoes:Rect <0x000049e8>)" do
-      expect(rect.inspect).to match(/^\(Shoes:Rect <0x[0-9a-f]{8}>\)$/)
+    it "gives an #inspect like (Shoes:Klass <0x000049e8>)" do
+      expect(object.inspect).to match(/^\(Shoes:InspectableObject <0x[0-9a-f]{8}>\)$/)
     end
   end
 end
