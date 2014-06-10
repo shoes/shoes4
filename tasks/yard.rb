bc7a15e177174e754ec1439093d7757a30e4e449
Eric Watson <wasnotrice@gmail.com>
2014-06-10 00:02:56 -0500

    Add sample and yard task files.

---
+++ tasks/yard.rb
@@ -0,0 +1,13 @@
+begin
+  require 'yard'
+
+  YARD::Rake::YardocTask.new do |t|
+    t.options = ['-mmarkdown']
+  end
+rescue LoadError
+  desc "Generate YARD Documentation"
+  task :yard do
+    abort 'YARD is not available. Try: gem install yard'
+  end
+end
+
