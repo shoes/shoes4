ce050722432e1a7649d8f15b158f16fe07392b54
Jason R. Clark <jclark@newrelic.com>
2014-07-06 17:09:52 -0600

    Fix for intermittent test failures
    RedrawingAspects were being left around by this test which made other
    tests later on cranky

--- spec/swt_shoes/integration_spec.rb
+++ spec/swt_shoes/integration_spec.rb
@@ -10,9 +10,18 @@ describe Shoes::App do
   end
 
   it 'does not fail with just a simple para #574' do
-    expect do
-      app = Shoes.app do para 'me no fail' end
-      app.quit
-    end.not_to raise_error
+    app = nil
+    begin
+      expect do
+        app = Shoes.app do para 'me no fail' end
+        app.quit
+      end.not_to raise_error
+    ensure
+      # Really ugly, but we have to can't let the redrawing aspect persist out
+      # of this test run, don't really want to expose it broadly, and can't
+      # clear it during `quit` like might feel natural because of #782
+      aspect = app.instance_variable_get(:@__app__).gui.instance_variable_get(:@redrawing_aspect)
+      aspect.remove_redraws
+    end
   end
 end
