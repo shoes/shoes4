6cd6e1817b262953cf03be012df331e91572bdc6
George <george@george-pc.(none)>
2014-07-22 23:12:42 +0100

    Refactored with text_font class

--- lib/shoes/swt/text_block/text_font_factory.rb
+++ lib/shoes/swt/text_block/text_font_factory.rb
@@ -6,29 +6,24 @@ class Shoes
       end
 
       def create_font(font_style)
-        name = font_style[:name]
-        size = font_style[:size]
-        styles = font_style[:styles].reduce { |result, s| result | s }
 
-        # Check if this font already exists, use that instead if it does
-        for existing_font in @fonts
-          if [Shoes.display, name, size, styles] == existing_font[1..@fonts.length]
-            return existing_font[0]
-          end
-        end
-
-        font = ::Swt::Graphics::Font.new Shoes.display, name, size, styles
+		new_font = TextFont.new(Shoes.display, font_style, font = nil)
+		
+		# Check if font with same style and display exists
+		existing_font = @fonts.find{ |font| font == new_font }
+		return existing_font.font if existing_font
 
         # Hold a reference to the font so we can dispose it when the time comes
-        @fonts.push([font, Shoes.display, name, size, styles])
+		@fonts << new_font.make_font
 
-        font
+        new_font.font
       end
 
       def dispose
-        @fonts.each { |font, _, _, _, _| font.dispose unless font.disposed? }
+        @fonts.each { |text_font| text_font.font.dispose unless text_font.font.disposed? }
         @fonts.clear
       end
     end
   end
 end
+
