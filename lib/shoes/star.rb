f7f7e9072dd632eca7479283133653d4afe5eedf
KC Erb <iamkcerb@gmail.com>
2014-08-01 07:58:26 -0600

    Update star to use style

--- lib/shoes/star.rb
+++ lib/shoes/star.rb
@@ -1,16 +1,16 @@
 class Shoes
   class Star
     include Common::UIElement
-    include Common::Fill
-    include Common::Stroke
     include Common::Style
     include Common::Clickable
 
-    attr_reader :app, :gui, :angle, :dimensions, :outer, :inner, :points,
-                :parent
+    attr_reader :app, :parent, :dimensions, :gui
+    style_with :angle, :art_styles, :dimensions, :inner, :outer, :points
 
-    def initialize(app, parent, left, top, points, outer, inner, opts = {}, blk = nil)
+    def initialize(app, parent, left, top, points, outer, inner, styles = {}, blk = nil)
       @app = app
+      @parent = parent
+      style_init(styles, inner: inner, outer: outer, points: points)
 
       # Careful not to turn Fixnum to Float, lest Dimensions make you relative!
       width = outer*2
@@ -23,19 +23,13 @@ class Shoes
       inner_dimensions = AbsoluteDimensions.new 0, 0, inner*2, 0
 
       # Get actual outer/inner from the dimension to handle relative values
-      @outer = @dimensions.width / 2
-      @inner = inner_dimensions.width / 2
+      @style[:outer] = @dimensions.width / 2
+      @style[:inner] = inner_dimensions.width / 2
 
-      @points = points
-      @angle = opts[:angle] || 0
-      @style = Shoes::Common::Fill::DEFAULTS.merge(Shoes::Common::Stroke::DEFAULTS).merge(opts)
-      @style[:strokewidth] ||= 1
-      @parent = parent
       @parent.add_child self
-
       @gui = Shoes.backend_for(self)
 
-      register_click(opts, blk)
+      register_click(@style, blk)
     end
 
     def in_bounds?(x, y)
