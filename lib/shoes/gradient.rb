8aaf77f885166fe20618c69ad2f87b3982e689e2
KC Erb <iamkcerb@gmail.com>
2014-08-09 08:58:06 -0700

    updated gradient comparison

--- lib/shoes/gradient.rb
+++ lib/shoes/gradient.rb
@@ -1,6 +1,7 @@
 class Shoes
   class Gradient
     include Common::Inspect
+    include Comparable
 
     def initialize(color1, color2, alpha = Shoes::Color::OPAQUE)
       @color1, @color2 = color1, color2
@@ -16,16 +17,10 @@ class Shoes
     def <=>(other) #arbitrarily compare 1st non-equal color
       raise_class_mismatch_error(other) unless other.is_a?(self.class)
       if @color1 == other.color1
-        compare_colors(@color2,other.color2)
+        @color2 <=> other.color2
       else
-        compare_colors(@color1, other.color1)
+        @color1 <=> other.color1
       end
-    end
-
-    def compare_colors(color_a, color_b)
-      sum_a = color_a.red * 10**6 + color_a.blue * 10**3 + color_a.green + color_b.alpha
-      sum_b = color_b.red * 10**6 + color_b.blue * 10**3 + color_b.green + color_b.alpha
-      sum_a > sum_b ? 1 : -1
     end
 
     def raise_class_mismatch_error other
