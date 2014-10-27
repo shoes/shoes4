581082a6e8b3d3b5d6d19757b0a49226540240e4
Jason R. Clark <jclark@newrelic.com>
2014-10-26 23:11:32 -0700

    Don't need ruby-shoes anymore!

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -5,8 +5,8 @@ require 'fileutils'
 # Decide if user uninstalled executables based on our actual Ruby script.
 Gem.post_uninstall do |gem|
   uninstalling_shoes = gem.spec.name == "shoes"
-  missing_executable = !File.exists?(File.join(Gem.bindir, "ruby-shoes")) &&
-                       !File.exists?(File.join(Gem.bindir, "ruby-shoes.bat"))
+  missing_executable = !File.exists?(File.join(Gem.bindir, "shoes-picker")) &&
+                       !File.exists?(File.join(Gem.bindir, "shoes-picker.bat"))
 
   if uninstalling_shoes && missing_executable
     puts "Removing shoes"
