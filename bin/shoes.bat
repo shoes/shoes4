07e9055f8c4053e5516a3615553b052af9bd2246
Jason R. Clark <jclark@newrelic.com>
2014-11-24 23:06:37 -0800

    Attempt at getting shoes.bat lined up

--- bin/shoes.bat
+++ bin/shoes.bat
@@ -1,8 +1,8 @@
 @echo off
 
 REM Note that this is NOT the actual bin/shoes.bat that gets installed with
-REM the gem! ext\install\shoes.bat is copied over to bin for gems, but this
-REM is the right thing to use from bin on a source checkout.
+REM the gem! shoes\ext\install\shoes.bat is copied over to bin for gems, but
+REM this is the right thing to use from bin on a source checkout.
 
-set bin_dir=%~dp0
-ext\install\shoes.bat %*
+set bin_dir=shoes\bin
+shoes\ext\install\shoes.bat %*
--- shoes/bin/shoes.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Note that this is NOT the actual bin/shoes.bat that gets installed with
-REM the gem! ext\install\shoes.bat is copied over to bin for gems, but this
-REM is the right thing to use from bin on a source checkout.
-
-set bin_dir=%~dp0
-ext\install\shoes.bat %*
