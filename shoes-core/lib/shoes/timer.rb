55b87054388e621571ab1cc7fe1774b3cacc2fd9
Jason R. Clark <jclark@newrelic.com>
2014-11-29 00:09:23 -0800

    Hold onto current slot context for timer

--- shoes-core/lib/shoes/timer.rb
+++ shoes-core/lib/shoes/timer.rb
@@ -4,7 +4,15 @@ class Shoes
 
     def initialize(app, n = 1000, &blk)
       @app, @n, @blk = app, n, blk
-      @gui = Shoes.configuration.backend_for(self, @app.gui, @blk)
+
+      # We want to execute in the context of the slot where we were initially
+      # called, so capture that outside the proc and execute through that slot.
+      slot = @app.current_slot
+      wrapped_block = Proc.new do
+        slot.eval_block(@blk)
+      end
+
+      @gui = Shoes.configuration.backend_for(self, @app.gui, wrapped_block)
     end
 
     attr_reader :n, :gui
