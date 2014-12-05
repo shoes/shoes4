bf7497d1cf15855dfef718df30c985fbdf62dd6a
Jason R. Clark <jclark@newrelic.com>
2014-12-04 21:51:40 -0800

    Update Shoes::Timer to use slot's block binding

--- shoes-core/lib/shoes/timer.rb
+++ shoes-core/lib/shoes/timer.rb
@@ -3,16 +3,10 @@ class Shoes
     include Common::Inspect
 
     def initialize(app, n = 1000, &blk)
-      @app, @n, @blk = app, n, blk
-
-      # We want to execute in the context of the slot where we were initially
-      # called, so capture that outside the proc and execute through that slot.
-      slot = @app.current_slot
-      wrapped_block = Proc.new do
-        slot.eval_block(@blk)
-      end
-
-      @gui = Shoes.configuration.backend_for(self, @app.gui, wrapped_block)
+      @app = app
+      @n   = n
+      @blk = @app.current_slot.create_block_bound_to_slot(blk)
+      @gui = Shoes.configuration.backend_for(self, @app.gui, @blk)
     end
 
     attr_reader :n, :gui
