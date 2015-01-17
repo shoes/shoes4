b4a42e921f118c1cefb587f63b2934a8e78c3158
Jason R. Clark <jclark@newrelic.com>
2015-01-17 10:58:04 -0800

    Apply new Common::Initialization to Background

--- shoes-core/lib/shoes/background.rb
+++ shoes-core/lib/shoes/background.rb
@@ -1,20 +1,21 @@
 class Shoes
   class Background
+    include Common::Initialization
     include Common::UIElement
     include Common::BackgroundElement
     include Common::Style
 
     attr_reader :app, :parent, :dimensions, :gui
+
     style_with :angle, :common_styles, :curve, :dimensions, :fill
     STYLES = { angle: 0, curve: 0 }
 
-    def initialize(app, parent, color, styles = {})
-      @app = app
-      @parent = parent
-      style_init styles, fill: color
-      @dimensions = ParentDimensions.new parent, @style
-      @parent.add_child self
-      @gui = Shoes.backend_for self
+    def before_initialize(styles, color)
+      styles[:fill] = color
+    end
+
+    def create_dimensions(*_)
+      @dimensions = ParentDimensions.new @parent, @style
     end
   end
 end
