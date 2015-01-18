192188fe0e57d1df882556e5b8e8d810de2168bb
Jason R. Clark <jclark@newrelic.com>
2015-01-17 20:16:41 -0800

    Apply new Common::Initialization to Button

--- shoes-core/lib/shoes/button.rb
+++ shoes-core/lib/shoes/button.rb
@@ -1,20 +1,20 @@
 class Shoes
   class Button
+    include Common::Initialization
     include Common::UIElement
     include Common::Style
     include Common::Clickable
 
     attr_reader :app, :parent, :dimensions, :gui
+
     style_with :click, :common_styles, :dimensions, :state, :text
 
-    def initialize(app, parent, text, styles = {}, blk = nil)
-      @app = app
-      @parent = parent
-      style_init styles, text: text
-      @dimensions = Dimensions.new parent, @style
-      @parent.add_child self
-      @gui = Shoes.configuration.backend_for self, @parent.gui
-      register_click blk
+    def before_initialize(styles, text)
+      styles[:text] = text
+    end
+
+    def create_dimensions(*_)
+      @dimensions = Dimensions.new @parent, @style
     end
 
     def focus
