a152c18fa06ca9912d102395a09b461d671b3427
Jason R. Clark <jclark@newrelic.com>
2015-03-26 22:20:35 -0700

    Add benchmark-ips and use it for dimension benchmark

--- benchmark/dimension.rb
+++ benchmark/dimension.rb
@@ -1,21 +1,17 @@
-require 'benchmark'
+require 'benchmark/ips'
 
-Benchmark.bm do |benchmark|
+Benchmark.ips do |benchmark|
   dimension = Shoes::Dimension.new
   dimension.start  = 0
   dimension.extent = 10
   dimension.margin_start = 2
 
-  benchmark.report '1_000_000x margin_start' do
-    1_000_000.times do
-      dimension.margin_start
-    end
+  benchmark.report 'margin_start' do
+    dimension.margin_start
   end
 
-  benchmark.report '1_000_000x element_extent' do
-    1_000_000.times do
-      dimension.element_extent
-    end
+  benchmark.report 'element_extent' do
+    dimension.element_extent
   end
 end
 
