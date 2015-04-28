b09fccdf214908984fbb778dd5e6e19b66c84f3e
Jason R. Clark <jclark@newrelic.com>
2015-04-27 22:40:24 -0700

    Basic specs for image pattern drawing

--- shoes-swt/spec/shoes/swt/image_pattern_spec.rb
+++ shoes-swt/spec/shoes/swt/image_pattern_spec.rb
@@ -37,4 +37,45 @@ describe Shoes::Swt::ImagePattern do
       subject.apply_as_fill(gc, applied_to)
     end
   end
+
+  describe "#draw_image_pattern" do
+    let(:partial) { 20 }
+    let(:full)    { 100 }
+
+    it "draws once if too small" do
+      allow(applied_to).to receive(:width)  { 10 }
+      allow(applied_to).to receive(:height) { 10 }
+
+      expect_drawn(0, 0)
+      subject.draw_image_pattern(gc, applied_to)
+    end
+
+    it "draws partial horizontally" do
+      allow(applied_to).to receive(:width)         { full + partial }
+      allow(applied_to).to receive(:element_right) { full + partial }
+
+      expect_drawn(0, 0,   width: full)
+      expect_drawn(100, 0, width: partial)
+      subject.draw_image_pattern(gc, applied_to)
+    end
+
+    it "draws partial vertically" do
+      allow(applied_to).to receive(:height)         { full + partial }
+      allow(applied_to).to receive(:element_bottom) { full + partial }
+
+      expect_drawn(0, 0,   height: full)
+      expect_drawn(0, 100, height: partial)
+      subject.draw_image_pattern(gc, applied_to)
+    end
+
+    def expect_drawn(left, top, opts={})
+      width  = opts[:width] || 100
+      height = opts[:height] || 100
+
+      expect(gc).to receive(:draw_image).with(swt_image, 0, 0,
+                                              width, height,
+                                              left, top,
+                                              width, height)
+    end
+  end
 end
