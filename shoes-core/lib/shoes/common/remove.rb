629683ecd7a8babf7e8d7a34bffc57959be0e921
Jason R. Clark <jclark@newrelic.com>
2015-05-18 23:31:07 -0700

    Fix for build break on slots not having `@app`

--- shoes-core/lib/shoes/common/remove.rb
+++ shoes-core/lib/shoes/common/remove.rb
@@ -5,7 +5,7 @@ class Shoes
         parent.remove_child self if parent
         gui.remove if gui && gui.respond_to?(:remove)
 
-        @app.remove_mouse_hover_control(self)
+        @app.remove_mouse_hover_control(self) if @app
       end
     end
   end
