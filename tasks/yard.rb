deb3e763014484c831278ca4b777f16125e10502
Jason R. Clark <jclark@newrelic.com>
2016-03-09 22:22:19 -0800

    Protect against errors when loading yard

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -9,4 +9,9 @@ rescue LoadError
   task :yard do
     abort 'YARD is not available. Try: gem install yard'
   end
+rescue StandardError => ex
+  desc "Generate YARD Documentation"
+  task :yard do
+    abort "YARD is not available. Error was '#{ex.message}'"
+  end
 end
