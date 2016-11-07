b3f7f0e9b9adec5c2397ea177de88c480bd63460
Jared Moody <jared@jaredmoody.com>
2016-11-06 16:10:04 -0800

    Rubocop: Fix all Style/PerlBackrefs

--- shoes-core/lib/shoes/ui/picker.rb
+++ shoes-core/lib/shoes/ui/picker.rb
@@ -74,8 +74,8 @@ class Shoes
       end
 
       def name_for_candidate(candidate)
-        /.*lib\/shoes\/(.*)\/generate-backend.rb/ =~ candidate
-        return "shoes-#{$1.tr('/', '-')}"
+        match = /.*lib\/shoes\/(.*)\/generate-backend.rb/.match(candidate)[1]
+        "shoes-#{match.tr('/', '-')}"
       end
 
       def write_backend(generator_file, bin_dir)
