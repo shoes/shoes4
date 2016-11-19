890189f70f2acbd49920f5ed7cd1099b7131bfaf
Jason R. Clark <jclark@newrelic.com>
2016-11-18 20:36:51 -0800

    Fix overlapping fill on rounded rectangles
    Integer math being slightly off between inner fill and outer draw on
    borders sometimes let a single pixel show in the corners, which looked
    ugly.

--- shoes-swt/lib/shoes/swt/rect_painter.rb
+++ shoes-swt/lib/shoes/swt/rect_painter.rb
@@ -1,13 +1,18 @@
 class Shoes
   module Swt
     class RectPainter < Common::Painter
-      def fill(graphics_context)
-        graphics_context.fill_round_rectangle(@obj.element_left,
-                                              @obj.element_top,
-                                              @obj.element_width,
-                                              @obj.element_height,
-                                              @obj.corners * 2,
-                                              @obj.corners * 2)
+      def fill(gc)
+        # If drawing a stroke around the shape, inset the fill so the draw
+        # isn't inside bounds of fill because of integer division remainders.
+        stroke_width = @obj.dsl.style[:strokewidth] || 0
+        bump_by = stroke_width == 1 ? 1 : stroke_width / 2
+
+        gc.fill_round_rectangle(@obj.element_left + bump_by,
+                                @obj.element_top + bump_by,
+                                @obj.element_width - stroke_width,
+                                @obj.element_height - stroke_width,
+                                @obj.corners * 2,
+                                @obj.corners * 2)
       end
 
       def draw(gc)
