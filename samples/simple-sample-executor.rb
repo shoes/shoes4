c67f3900190a1ff79050f460508a12f624220024
Jared Moody <jared@jaredmoody.com>
2016-11-22 00:22:36 -0800

    Rubocop: Fix all Style/FileName

--- samples/simple-sample-executor.rb
+++
@@ -1,5 +0,0 @@
-Shoes.app width: 650, height: 32 do
-  %w(simple-altered-para simple-border-image simple-breadsticks simple-buttons).each do |name|
-    button(name) { load "samples/#{name}.rb" }
-  end
-end
