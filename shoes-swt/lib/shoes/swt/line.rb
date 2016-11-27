92fc7209b92dede9536d81803ebc811f77302ad5
Jason R. Clark <jclark@newrelic.com>
2016-11-27 09:16:53 -0800

    Move painters back momentarily for diff'ing purposes

--- shoes-swt/lib/shoes/swt/line.rb
+++ shoes-swt/lib/shoes/swt/line.rb
@@ -24,6 +24,19 @@ class Shoes
       def angle
         @dsl.angle
       end
+
+      class LinePainter < Common::Painter
+        def draw(gc)
+          gc.draw_line(@obj.dsl.translate_left + @obj.element_left,
+                       @obj.dsl.translate_top + @obj.element_top,
+                       @obj.dsl.translate_left + @obj.element_right + 1,
+                       @obj.dsl.translate_top + @obj.element_bottom + 1)
+        end
+
+        # Don't do fill setup
+        def fill_setup(_gc)
+        end
+      end
     end
   end
 end
--- shoes-swt/lib/shoes/swt/shape_painter.rb
+++ shoes-swt/lib/shoes/swt/shape_painter.rb
@@ -1,13 +1,4 @@
 class Shoes
   module Swt
-    class ShapePainter < Common::Painter
-      def fill(gc)
-        gc.fill_path(@obj.element)
-      end
-
-      def draw(gc)
-        gc.draw_path(@obj.element)
-      end
-    end
   end
 end
