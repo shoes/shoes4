950f5d49d74635f0121f25993ac99595e85d5bee
Jason R. Clark <jclark@newrelic.com>
2016-11-27 21:27:16 -0800

    Sample for translate

---
+++ samples/simple-translate.rb
@@ -0,0 +1,9 @@
+Shoes.app do
+  para "The following art elements would have shown in the top corner if not for translate!", center: true, size: 20
+
+  translate 100, 100
+  rect 0, 0, 100, 100, fill: blue
+
+  translate 200, 200
+  oval 0, 0, 100, 100, fill: green
+end
