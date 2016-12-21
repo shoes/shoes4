c146c47b077a018877b8156614a8f07d584efade
Jason R. Clark <jclark@newrelic.com>
2016-12-20 20:08:22 -0800

    Styling on block

--- shoes-core/spec/shoes/shared_examples/clickable.rb
+++ shoes-core/spec/shoes/shared_examples/clickable.rb
@@ -4,16 +4,12 @@ shared_examples "clickable object" do
   it { is_expected.to respond_to :register_click }
 
   it "can chain click" do
-    result = subject.click do
-    end
-
+    result = subject.click {}
     expect(result).to eq(subject)
   end
 
   it "can chain release" do
-    result = subject.release do
-    end
-
+    result = subject.release {}
     expect(result).to eq(subject)
   end
 end
