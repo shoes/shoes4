7d40a609902d78512f8b18b080bb84b98debfc52
Jason R. Clark <jason@jasonrclark.net>
2016-12-28 10:00:57 -0800

    Don't directly call jruby from shoes.bat
    Opens the way for us to do Ruby platform checking in shoes-picker since
    nothing before it is platform specific.

---
+++ bin/shoes-picker.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Note that this is NOT the actual shoes-picker.bat installed with shoes!
+REM Rubygems generates stubs on gem installation, so this takes the place of
+REM that when running from source.
+
+REM TODO: Would be nice to get rid of the direct jruby call, but lacking the
+REM generated stubs, we've got to get specific instead.
+jruby shoes-core\bin\shoes-picker bin
