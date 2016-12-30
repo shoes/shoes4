f5092d73611c6749318df1331573de30a1b0114e
Jason R. Clark <jason@jasonrclark.net>
2016-12-29 20:42:46 -0800

    Further explanatory comments for module inclusion

--- shoes-core/lib/shoes/common/art_element.rb
+++ shoes-core/lib/shoes/common/art_element.rb
@@ -9,6 +9,9 @@ class Shoes
       include Common::Translate
 
       # Modules that muck with class methods need to be included like this
+      #
+      # We also can't rely on Common::UIElement providing these, as it gets
+      # ArtElement, not the destination class to add things to!
       def self.included(base)
         base.include Common::Hover
         base.include Common::Style
--- shoes-core/lib/shoes/common/background_element.rb
+++ shoes-core/lib/shoes/common/background_element.rb
@@ -4,6 +4,9 @@ class Shoes
       include Common::UIElement
 
       # Modules that muck with class methods need to be included like this
+      #
+      # We also can't rely on Common::UIElement providing these, as it gets
+      # BackgroundElement, not the destination class to add things to!
       def self.included(base)
         base.include Common::Style
       end
