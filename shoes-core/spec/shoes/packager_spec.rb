6c3dfb6df8fd88c8c7f08464a8e646e4bf8a4353
Jason R. Clark <jason@jasonrclark.net>
2017-02-28 20:30:01 -0800

    Pick up gems from Bundler
    If we detect the presence of Bundler when packaging, we'll try to use
    its list of gems (minus 'shoes' which causes us problems and isn't
    necessary to install). This allows Rubyists to use the typical means of
    declaring their dependencies in a Ruby app, but works nicely even
    without a Gemfile around too.

--- shoes-core/spec/shoes/packager_spec.rb
+++ shoes-core/spec/shoes/packager_spec.rb
@@ -9,6 +9,18 @@ describe Shoes::Packager do
     subject.create_package("program", "swt:app")
   end
 
+  if defined?(::Bundler)
+    it "detects Bundler and includes gems" do
+      expect(subject.backend.gems).to_not be_empty
+      expect(subject.backend.gems).to include("shoes-core")
+    end
+
+    it "detects Bundler and includes gems" do
+      allow(::Bundler).to receive(:environment).and_return(nil)
+      expect(subject.backend.gems).to be_empty
+    end
+  end
+
   it "knows to run packaging if it created one" do
     subject.create_package("program", "swt:app")
     expect(subject.should_package?).to eq(true)
