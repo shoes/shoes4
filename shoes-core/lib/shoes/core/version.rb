7c541ec547c43820a1d2aa70370c236b5a63e46d
Jason R. Clark <jason@jasonrclark.net>
2017-03-12 14:27:36 -0700

    Version bump to 4.0.0.pre9

--- shoes-core/lib/shoes/core/version.rb
+++ shoes-core/lib/shoes/core/version.rb
@@ -2,6 +2,6 @@
 
 class Shoes
   module Core
-    VERSION = "4.0.0.pre8"
+    VERSION = "4.0.0.pre9"
   end
 end
--- shoes-swt/lib/shoes/swt/version.rb
+++ shoes-swt/lib/shoes/swt/version.rb
@@ -2,6 +2,6 @@
 
 class Shoes
   module Swt
-    VERSION = "4.0.0.pre8"
+    VERSION = "4.0.0.pre9"
   end
 end
