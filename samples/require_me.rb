26f2615caeed4427b012496f2ff1a39f483ec0f0
Jason R. Clark <jason@jasonrclark.net>
2017-05-07 12:41:43 -0700

    Rubocop fixes and include new sample in main list

--- samples/require_me.rb
+++ samples/require_me.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class RequireMe
   def self.message
     "This came from another file we required"
--- samples/simple_require.rb
+++ samples/simple_require.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+#
 # This app tests that we are correctly setting load paths so files in the
 # executing directory can be required withour require_relative shenanigans.
 require 'require_me'
