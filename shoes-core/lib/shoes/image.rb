0989d77d09655aa39462d49b0d632d68901da40e
Jason R. Clark <jason@jasonrclark.net>
2017-05-14 10:24:55 -0700

    Search paths for image
    Try to look in Dir.pwd, then the app dir (where our Shoes app started),
    then finally the shoes-core static directory.

--- shoes-core/lib/shoes/image.rb
+++ shoes-core/lib/shoes/image.rb
@@ -40,9 +40,26 @@ class Shoes
     end
 
     def absolute_file_path(path)
-      path = File.join(Dir.pwd, path) unless Pathname(path).absolute?
-      raise FileNotFoundError, "#{path} not found." unless File.exist?(path)
-      path
+      if Pathname(path).absolute?
+        search_for(File.basename(path), File.dirname(path))
+      else
+        search_for(path, *default_search_paths)
+      end
+    end
+
+    def search_for(path, *locations)
+      found = locations.map  { |dir| File.join(dir, path) }
+                       .find { |candidate| File.exist?(candidate) }
+
+      unless found
+        raise FileNotFoundError, "#{path} not found. Searched #{locations.join(',')}"
+      end
+
+      found
+    end
+
+    def default_search_paths
+      [Dir.pwd, Shoes.configuration.app_dir, File.join(Shoes::DIR, "static")]
     end
   end
 end
