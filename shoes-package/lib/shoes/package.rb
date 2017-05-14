7b5a8a6fcd1b6b8dae5c2e2125c3106d5cdc5f19
Jason R. Clark <jason@jasonrclark.net>
2017-05-14 15:45:56 -0700

    Revise to use new MacApp class

--- shoes-package/lib/shoes/package.rb
+++ shoes-package/lib/shoes/package.rb
@@ -3,14 +3,14 @@ class Shoes
   module Package
     def self.create_packager(config, package_type)
       require 'furoshiki/jar'
-      require 'furoshiki/jar_app'
+      require 'furoshiki/mac_app'
       require 'furoshiki/windows_app'
 
       case package_type
       when :jar
         ::Furoshiki::Jar.new(config)
       when :mac
-        ::Furoshiki::JarApp.new(config)
+        ::Furoshiki::MacApp.new(config)
       when :windows
         ::Furoshiki::WindowsApp.new(config)
       else
