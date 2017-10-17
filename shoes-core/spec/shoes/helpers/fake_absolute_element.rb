a76f969187bdeead9ce4ce3bf366dab1e8d46a19
Jason R. Clark <jason@jasonrclark.net>
2017-10-16 21:08:05 -0700

    Fix core specs

--- shoes-core/spec/shoes/helpers/fake_absolute_element.rb
+++ shoes-core/spec/shoes/helpers/fake_absolute_element.rb
@@ -1,32 +1,12 @@
 # frozen_string_literal: true
 class Shoes
-  class FakeAbsoluteElement
-    include Common::Attachable
-    include Common::Inspect
-    include Common::Positioning
-    include Common::Remove
-    include Common::Visibility
-
+  class FakeAbsoluteElement < FakeElement
     include Shoes::DimensionsDelegations
+
+    attr_accessor :dimensions
 
     def initialize
       @dimensions = AbsoluteDimensions.new 0, 0, 100, 100
     end
-
-    def add_child(_element)
-      true
-    end
-
-    def adjust_current_position(*_)
-    end
-
-    # Fake this out instead of using Common::Style to avoid things like touching
-    # app level styles, etc. that we don't need for testing purposes
-    def style
-      @style ||= {}
-      @style
-    end
-
-    attr_accessor :dimensions, :parent, :gui
   end
 end
