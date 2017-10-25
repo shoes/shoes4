127ef44473785c32015ca73cc4593ea72a91a9e5
Jason R. Clark <jason@jasonrclark.net>
2017-10-24 21:54:42 -0700

    Apply scrolling to oval

--- shoes-swt/spec/shoes/swt/oval_painter_spec.rb
+++ shoes-swt/spec/shoes/swt/oval_painter_spec.rb
@@ -38,4 +38,10 @@ describe Shoes::Swt::OvalPainter do
     expect(gc).to receive(:draw_oval).with(left + sw / 2, top + sw / 2, width - sw, height - sw)
     subject.paint_control(event)
   end
+
+  it "positions correctly when scrolled" do
+    allow(parent).to receive(:scroll_top).and_return(10)
+    expect(gc).to receive(:draw_oval).with(left + sw / 2, top - 10 + sw / 2, width - sw, height - sw)
+    subject.paint_control(event)
+  end
 end
