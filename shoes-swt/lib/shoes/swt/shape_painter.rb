087f2ac5e2da220356030670b42d447e63b5138f
Jason R. Clark <jason@jasonrclark.net>
2017-10-27 21:05:13 -0700

    Scroll shapes

--- shoes-swt/lib/shoes/swt/shape_painter.rb
+++ shoes-swt/lib/shoes/swt/shape_painter.rb
@@ -2,6 +2,17 @@
 class Shoes
   module Swt
     class ShapePainter < Common::Painter
+      def before_painted
+        if @obj.scroll_top_applied != @obj.dsl.parent.scroll_top
+          # Put back what we've already done
+          @obj.transform.translate(0, @obj.scroll_top_applied) if @obj.scroll_top_applied
+
+          # Move it!
+          @obj.transform.translate(0, -@obj.dsl.parent.scroll_top)
+          @obj.scroll_top_applied = @obj.dsl.parent.scroll_top
+        end
+      end
+
       def fill(gc)
         gc.fill_path(@obj.element)
       end
--- shoes-swt/spec/shoes/swt/shape_painter_spec.rb
+++ shoes-swt/spec/shoes/swt/shape_painter_spec.rb
@@ -6,7 +6,8 @@ describe Shoes::Swt::ShapePainter do
   include_context "painter context"
 
   let(:dsl) do
-    double("Shoes::Shape", hidden: false, needs_rotate?: false, style: {}).as_null_object
+    double("Shoes::Shape", parent: parent, hidden: false,
+                           needs_rotate?: false, style: {}).as_null_object
   end
 
   let(:shape) { Shoes::Swt::Shape.new(dsl, swt_app) }
@@ -24,5 +25,14 @@ describe Shoes::Swt::ShapePainter do
   it "draws path" do
     expect(gc).to receive(:draw_path)
     subject.paint_control(event)
+  end
+
+  it "remembers scrolling position applied" do
+    expect(shape.scroll_top_applied).to eq(nil)
+
+    allow(parent).to receive(:scroll_top).and_return(10)
+    subject.paint_control(event)
+
+    expect(shape.scroll_top_applied).to eq(10)
   end
 end
