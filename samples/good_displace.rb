be0996a707feb98896e1e31a7973e890b7710734
Jason R. Clark <jason@jasonrclark.net>
2017-11-18 12:55:50 -0600

    Make edit box a little smaller

--- samples/good_displace.rb
+++ samples/good_displace.rb
@@ -26,7 +26,7 @@ class Layout
   end
 
   def edit_box
-    @app.edit_box "Edit me", left: left(:edit_box), top: top, width: width
+    @app.edit_box "Edit me", left: left(:edit_box), top: top, height: 50, width: width
   end
 
   def offset(element)
