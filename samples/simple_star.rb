42c348374da703047248f10efdada9c0e131134c
nichol alexander <nichol.alexander@gmail.com>
2017-11-30 15:02:08 -0500

    a nicer sample program that also follows you around - fun!

--- samples/simple_star.rb
+++ samples/simple_star.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 # a simple shoes app to show how to use the star.center_point method
-Shoes.app do
-  fill rgb(0, 0.6, 0.9, 0.1)
+Shoes.app width: 600, height: 600 do
+  fill rgb(158, 223, 229, 0.1)
   stroke rgb(0, 0.6, 0.9)
   strokewidth 0.25
+  @pleione = star 200, 100
 
-  blurgh = star 200, 200
-
-  center_point = blurgh.center_point
-
+  # draw two lines that intersect the center of the star
+  center_point = @pleione.center_point
   line center_point.x, 0, center_point.x, 500
   line 0, center_point.y, 500, center_point.y
 
+  # draw a small circle at the center of the star
   radius = 3
   oval (center_point.x - radius), (center_point.y - radius), radius: radius
+
+  # do you want a star to follow you around?
+  @churyumov_gerasimenko = star 200, 100
+  motion do |x, y|
+    @churyumov_gerasimenko.center_point = Shoes::Point.new(x, y)
+  end
 end
