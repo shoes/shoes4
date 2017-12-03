5070e3c6a2d87eb5a65c6bcebb70d503e684a0ff
Jared Moody <jared@jaredmoody.com>
2017-12-02 16:43:41 -0800

    Rubocop: Lint/AmbiguousBlockAssociation

--- shoes-swt/spec/shoes/swt/shared_examples/clickable.rb
+++ shoes-swt/spec/shoes/swt/shared_examples/clickable.rb
@@ -3,12 +3,12 @@
 shared_examples 'clickable backend' do
   describe 'interaction with the swt app object' do
     it 'adds a listener for the MouseDown event when click is called' do
-      subject.click proc {}
+      subject.click(proc {})
       expect(click_listener).to have_received(:add_click_listener).at_least(1)
     end
 
     it 'adds a listener for the MouseUp event when release is called' do
-      subject.release proc {}
+      subject.release(proc {})
       expect(click_listener).to have_received(:add_release_listener).at_least(1)
     end
   end
