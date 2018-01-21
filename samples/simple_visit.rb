58cd175038c42b7959954a9b94204c0cf2c99d89
Jason R. Clark <jason@jasonrclark.net>
2018-01-20 22:00:04 -0800

    Add a visit-based sample

---
+++ samples/simple_visit.rb
@@ -0,0 +1,9 @@
+class AppComponent < Shoes
+  url '/', :index
+
+  def index
+    alert("Built-in methods are available in visit methods")
+  end
+end
+
+Shoes.app width: 800, height: 600, title: 'testing page'
