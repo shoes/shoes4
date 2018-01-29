c8c0d0b7e6907ad71ba05204d1b4bc485327678a
Jason R. Clark <jclark@newrelic.com>
2018-01-28 22:06:00 -0800

    Clean and rebuild tasks for YARD

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -15,3 +15,13 @@ rescue StandardError => ex
     abort "YARD is not available. Error was '#{ex.message}'"
   end
 end
+
+namespace :yard do
+  desc "Clean up YARD documentation folder"
+  task :clean do
+    sh("rm -rf ./doc")
+  end
+
+  desc "Clean and build"
+  task :all => [:'yard:clean', :'yard']
+end
