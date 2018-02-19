533bab5bd31184077e2b1c0e50ede56823ec868f
Jason R. Clark <jason@jasonrclark.net>
2018-02-19 10:58:44 -0800

    Add centered text to make lorem more useful

--- samples/simple_lorem_ipsum.rb
+++ samples/simple_lorem_ipsum.rb
@@ -8,6 +8,8 @@ TEXT = <<~EOS
   reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
       Excepteur sint occaecat cupidatat non proident, sunt in \
   culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 EOS
 
 NL = "\n\n"
@@ -17,4 +19,5 @@ Shoes.app do
   para lines[0], NL
   para lines[1], NL, justify: true
   para lines[2], NL, leading: 0
+  para lines[3], NL, align: 'center'
 end
