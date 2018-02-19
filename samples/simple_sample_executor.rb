f142d1d2f64ffe3cd5c66c2838756987ffe3a1a5
Jason R. Clark <jason@jasonrclark.net>
2018-02-19 11:25:40 -0800

    Remove simple executor, let the full one be our sample

--- samples/simple_sample_executor.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Shoes.app width: 650, height: 32 do
-  path = File.expand_path(File.join(__FILE__, ".."))
-  %w[simple_altered_para simple_border_image simple_breadsticks simple_buttons].each do |name|
-    button(name) { load "#{path}/#{name}.rb" }
-  end
-end
